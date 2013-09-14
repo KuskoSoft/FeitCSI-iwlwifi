1224911b015fc521591ea19620975f9d6924dd8b
Eytan Lifshitz <eytan.lifshitz@intel.com>
2013-09-14 15:11:05 +0200

    iwlwifi: mvm: fix inverted if condition
    In case we have an external NVM file, we don't power up the NIC
    at driver load time. Therefore, we don't need to power it down
    in case of failure.
    The if condition which have checked that was inverted. fixed.
    
    Change-Id: I3c37cd4a908e4544ed52b1056bd7304b5972c762
    Signed-off-by: Eytan Lifshitz <eytan.lifshitz@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/22543
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Tested-by: IWL Jenkins
    
    x-iwlwifi-stack-dev: 1bb5bcaea18ca8b147bfc3e03f48f29ed7e4319d

--- drivers/net/wireless/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/iwlwifi/mvm/ops.c
@@ -476,7 +476,7 @@ iwl_op_mode_mvm_start(struct iwl_trans *trans, const struct iwl_cfg *cfg,
  out_free:
 	iwl_phy_db_free(mvm->phy_db);
 	kfree(mvm->scan_cmd);
-	if (iwlwifi_mod_params.nvm_file)
+	if (!iwlwifi_mod_params.nvm_file)
 		iwl_trans_stop_hw(trans, true);
 	ieee80211_free_hw(mvm->hw);
 	return NULL;
