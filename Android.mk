f7032e0ffbf7da5fdd8fd498ee64d02196fb1a01
Benzi Zbit <benzi.zbit@intel.com>
2013-10-02 15:54:55 +0200

    iwlwifi: Clean up and update Android.mk.
    In order to have one Android.mk for all Android
    platforms, some variables introduced.
    When all the needed changes will go in into DRD
    and OTC trees, OTC specific lines and
    Androidiwlwifi.mk will be removed.
    
    Change-Id: I3166a430db26cc6b423890b00f041b3d0410cadd
    Signed-off-by: Benzi Zbit <benzi.zbit@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/23567
    
    x-iwlwifi-stack-dev: 686744c7c6059d33208c74c1ce07f75976c79d6e

--- Android.mk
+++ Android.mk
@@ -9,23 +9,55 @@
 #
 
 # for integrated compat build
+# ** The below two lines is OTC tree specific & this will be addressed/removed shortly **
 COMPAT_KERNEL_MODULES += external/intel_iwlwifi
 
 ifeq ($(INTEL_COMPAT_INTEGRATED_BUILD),)
 
-IWLWIFI_COMPAT_DIR := $(call my-dir)
+# Run only this build if variant define the needed configuration
+# e.g. Enabling iwlwifi for XMM6321
+# BOARD_USING_INTEL_IWL := true      - this will enable iwlwifi building
+# INTEL_IWL_BOARD_CONFIG := xmm6321  - the configuration, defconfig-xmm6321
+# INTEL_IWL_USE_COMPAT_INSTALL := y  - this will use kernel modules installation
+# INTEL_IWL_COMPAT_INSTALL_DIR := updates - the folder that the modules will be installed in
+ifeq ($(BOARD_USING_INTEL_IWL),true)
 
-IWLWIFI_BUILD_COMPAT: $(INSTALLED_KERNEL_TARGET)
-	@echo iwlwifi-dev building iwlwifi-dev stack
-	@echo $(mk_iwlwifi_compat) KLIB_BUILD=$(KBUILD_OUTPUT)
-	$(MAKE) -C $(IWLWIFI_COMPAT_DIR) ARCH=$(TARGET_ARCH) KLIB_BUILD=$(KBUILD_OUTPUT)
+.PHONY: iwlwifi
 
-IWLWIFI_INSTALL_COMPAT: IWLWIFI_BUILD_COMPAT
-	@echo iwlwifi-dev installing iwlwifi-dev stack
-	$(MAKE) -C $(KBUILD_OUTPUT) M=$(PWD)/$(IWLWIFI_COMPAT_DIR) INSTALL_MOD_DIR=updates INSTALL_MOD_PATH=$(PWD)/$(TARGET_OUT) modules_install
+INTEL_IWL_SRC_DIR := $(call my-dir)
+INTEL_IWL_OUT_DIR := $(ANDROID_BUILD_TOP)/$(PRODUCT_OUT)/iwlwifi
 
+ifeq ($(TARGET_ARCH),arm)
+ifeq ($(CROSS_COMPILE),)
+$(warning You are building for an ARM platform, but no CROSS_COMPILE is set. This is likely an error.)
+endif
+endif
 
-.PHONY: iwlwifi-dev
-iwlwifi-dev: IWLWIFI_INSTALL_COMPAT
+ifeq ($(INTEL_IWL_USE_COMPAT_INSTALL),y)
+INTEL_IWL_COMPAT_INSTALL := iwlwifi_install
+else
+# use system install
+copy_modules_to_root: iwlwifi
+ALL_KERNEL_MODULES += $(INTEL_IWL_OUT_DIR)
+endif
 
+iwlwifi: iwlwifi_build $(INTEL_IWL_COMPAT_INSTALL)
+
+iwlwifi_copy:
+	@mkdir -p $(INTEL_IWL_OUT_DIR)
+	@cp -rfl $(INTEL_IWL_SRC_DIR)/. $(INTEL_IWL_OUT_DIR)/
+
+iwlwifi_configure: $(INSTALLED_KERNEL_TARGET) iwlwifi_copy
+	$(info Configuring kernel module iwlwifi with defconfig-$(INTEL_IWL_BOARD_CONFIG))
+	$(MAKE) -C $(INTEL_IWL_OUT_DIR)/ ARCH=$(TARGET_ARCH) $(CROSS_COMPILE) KLIB_BUILD=$(ANDROID_BUILD_TOP)/$(KERNEL_OUT_DIR) defconfig-$(INTEL_IWL_BOARD_CONFIG)
+
+iwlwifi_build: iwlwifi_configure
+	$(info Building kernel module iwlwifi in $(INTEL_IWL_OUT_DIR))
+	$(MAKE) -C $(INTEL_IWL_OUT_DIR)/ ARCH=$(TARGET_ARCH) $(CROSS_COMPILE) KLIB_BUILD=$(ANDROID_BUILD_TOP)/$(KERNEL_OUT_DIR)
+
+iwlwifi_install: iwlwifi_build
+	$(info Installing kernel modules in $(ANDROID_BUILD_TOP)/$(TARGET_OUT))
+	$(MAKE) -C $(ANDROID_BUILD_TOP)/$(KERNEL_OUT_DIR) M=$(INTEL_IWL_OUT_DIR)/ INSTALL_MOD_DIR=$(INTEL_IWL_COMPAT_INSTALL_DIR) INSTALL_MOD_PATH=$(ANDROID_BUILD_TOP)/$(TARGET_OUT) modules_install
+
+endif
 endif
