fa27cb64f1c75029ab9b38c844849545c7057850
Oren Givon <oren.givon@intel.com>
2013-12-11 07:37:06 +0200

    iwlwifi: update Android.mk to fix android DRD build
    A required update for Android.mk to fix some android
    builds. This update adds a workaround to remove the
    kernel's mac80211.ko and cfg80211.ko files.
    
    Change-Id: Icac641b8bcd5093d544958a2ef9c71744a92a4ef
    Signed-off-by: Oren Givon <oren.givon@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/27023
    Tested-by: IWL Jenkins
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    
    x-iwlwifi-stack-dev: 9fe3b1310a7d0b294ba4107f1ff5dc11c1cd78c4

--- Android.mk
+++ Android.mk
@@ -45,6 +45,14 @@ ALL_KERNEL_MODULES += $(INTEL_IWL_OUT_DIR)
 INTEL_IWL_KERNEL_DEPEND := build_bzImage
 endif
 
+ifeq ($(INTEL_IWL_USE_RM_MAC_CFG),y)
+copy_modules_to_root: iwlwifi
+INTEL_IWL_COMPAT_INSTALL_PATH := $(ANDROID_BUILD_TOP)/$(KERNEL_OUT_MODINSTALL)
+INTEL_IWL_KERNEL_DEPEND := modules_install
+INTEL_IWL_RM_MAC_CFG_DEPEND := iwlwifi_rm_mac_cfg
+INTEL_IWL_INSTALL_MOD_STRIP := INSTALL_MOD_STRIP=1
+endif
+
 # check if the modules.dep file should be edited and fixed with scripts
 ifeq ($(INTEL_IWL_EDIT_MOD_DEP),y)
 iwlwifi_install: iwlwifi_save_mod_dep
@@ -68,9 +76,14 @@ iwlwifi_build: iwlwifi_configure
 	@$(info Building kernel module iwlwifi in $(INTEL_IWL_OUT_DIR))
 	@$(MAKE) -C $(INTEL_IWL_OUT_DIR)/ ARCH=$(TARGET_ARCH) $(CROSS_COMPILE) KLIB_BUILD=$(ANDROID_BUILD_TOP)/$(KERNEL_OUT_DIR)
 
-iwlwifi_install: iwlwifi_build
+iwlwifi_install: iwlwifi_build $(INTEL_IWL_RM_MAC_CFG_DEPEND)
 	@$(info Installing kernel modules in $(INTEL_IWL_COMPAT_INSTALL_PATH))
-	@$(MAKE) -C $(ANDROID_BUILD_TOP)/$(KERNEL_OUT_DIR) M=$(INTEL_IWL_OUT_DIR)/ INSTALL_MOD_DIR=$(INTEL_IWL_COMPAT_INSTALL_DIR) INSTALL_MOD_PATH=$(INTEL_IWL_COMPAT_INSTALL_PATH) modules_install
+	@$(MAKE) -C $(ANDROID_BUILD_TOP)/$(KERNEL_OUT_DIR) M=$(INTEL_IWL_OUT_DIR)/ INSTALL_MOD_DIR=$(INTEL_IWL_COMPAT_INSTALL_DIR) INSTALL_MOD_PATH=$(INTEL_IWL_COMPAT_INSTALL_PATH) $(INTEL_IWL_INSTALL_MOD_STRIP) modules_install
+
+iwlwifi_rm_mac_cfg: iwlwifi_build
+	$(info Remove kernel cfg80211.ko and mac80211.ko)
+	@find $(KERNEL_OUT_MODINSTALL)/lib/modules/ -name "mac80211.ko" | xargs rm -f
+	@find $(KERNEL_OUT_MODINSTALL)/lib/modules/ -name "cfg80211.ko" | xargs rm -f
 
 iwlwifi_save_mod_dep:
 	@find $(INTEL_IWL_COMPAT_INSTALL_PATH) -name modules.dep -exec cp {} $(INTEL_IWL_OUT_DIR)/modules.dep.orig \;
