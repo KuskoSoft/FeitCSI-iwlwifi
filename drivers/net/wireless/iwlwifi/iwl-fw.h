bf5abc955f2b7a557a3ce656c8d547384abbaea8
David Spinadel <david.spinadel@intel.com>
2014-02-27 16:41:52 +0200

    iwlwifi: mvm: support extended beacon notification
    Use extended beacon notification when supported by FW.
    Set last beacon system time to AP or GO interface.
    System time of last beacon can be used to avoid TBTT overlapping
    between two interfaces, CSA and other uses.
    
    Change-Id: I46dfba2e3d98149c792617e2660ade68430e4248
    Signed-off-by: David Spinadel <david.spinadel@intel.com>
    Signed-off-by: Andrei Otcheretianski <andrei.otcheretianski@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/31872
    Tested-by: IWL Jenkins
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    
    x-iwlwifi-stack-dev: 948aee252c5df8add69bd0bd8cdfb39677d6a5d9

--- drivers/net/wireless/iwlwifi/iwl-fw.h
+++ drivers/net/wireless/iwlwifi/iwl-fw.h
@@ -118,10 +118,12 @@ enum iwl_ucode_tlv_flag {
 /**
  * enum iwl_ucode_tlv_api - ucode api
  * @IWL_UCODE_TLV_API_WOWLAN_CONFIG_TID: wowlan config includes tid field.
+ * @IWL_UCODE_TLV_CAPA_EXTENDED_BEACON: Support Extended beacon notification
  * @IWL_UCODE_TLV_API_CSA_FLOW: ucode can do unbind-bind flow for CSA.
  */
 enum iwl_ucode_tlv_api {
 	IWL_UCODE_TLV_API_WOWLAN_CONFIG_TID	= BIT(0),
+	IWL_UCODE_TLV_CAPA_EXTENDED_BEACON	= BIT(1),
 	IWL_UCODE_TLV_API_CSA_FLOW		= BIT(4),
 };
 
