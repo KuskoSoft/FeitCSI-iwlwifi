721c415488466f8ef7ef9e41cefaa693ba6f7292
Eran Harary <eran.harary@intel.com>
2014-06-01 14:55:21 +0300

    iwlwifi: mvm: load nvm_file only in sdio and change the iwl8260 struct name
    1. nvm_file should load by default only in sdio products
    2. fix typo in iwl8260 struct name: it's 11ac and not 11n
    
    Change-Id: Ia7a503ab14b4ce597c05ebc7a44904edd2121513
    Signed-off-by: Eran Harary <eran.harary@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/38152
    Reviewed-by: Liad Kaufman <liad.kaufman@intel.com>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Tested-by: IWL Jenkins
    
    x-iwlwifi-stack-dev: 456656c2881da53b28cf2df8570aaf701801a41a

--- drivers/net/wireless/iwlwifi/iwl-config.h
+++ drivers/net/wireless/iwlwifi/iwl-config.h
@@ -293,7 +293,7 @@ extern const struct iwl_cfg iwl7265_2ac_cfg;
 extern const struct iwl_cfg iwl7265_2n_cfg;
 extern const struct iwl_cfg iwl7265_n_cfg;
 extern const struct iwl_cfg iwl8260_2ac_cfg;
-extern const struct iwl_cfg iwl8260_n_cfg;
+extern const struct iwl_cfg iwl8260_2ac_sdio_cfg;
 #endif /* CPTCFG_IWLMVM */
 
 #endif /* __IWL_CONFIG_H__ */
