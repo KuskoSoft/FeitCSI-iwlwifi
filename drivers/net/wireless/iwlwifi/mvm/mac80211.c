c2d05874f6c1bbec0750dd63e90e9f7970a7792f
Luciano Coelho <luciano.coelho@intel.com>
2014-06-27 15:05:40 +0300

    iwlwifi: mvm: use switching_chanctx argument instead of csa_active
    Now that all CSA flows are using the switch_vif_chanctx op, we can
    rely on the switching_chanctx boolean that is passed to the
    __iwl_mvm_assign_vif_chanctx() and __iwl_mvm_unassign_vif_chanctx()
    functions to decide whether the context switch flows need to be
    executed.  In this way we make the chanctx switch flow more generic,
    without having to rely on the csa_active flag being set.
    
    Change-Id: If880b08a52038b3e789884b7cf60ce263acef59f
    Signed-off-by: Luciano Coelho <luciano.coelho@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/42232
    Tested-by: IWL Jenkins
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    
    x-iwlwifi-stack-dev: 73c692a1bac53f4c7cf80936431144b2c29c88b1

--- drivers/net/wireless/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/iwlwifi/mvm/mac80211.c
@@ -2812,8 +2812,8 @@ static int __iwl_mvm_assign_vif_chanctx(struct iwl_mvm *mvm,
 
 	switch (vif->type) {
 	case NL80211_IFTYPE_AP:
-		/* Unless it's a CSA flow we have nothing to do here */
-		if (vif->csa_active) {
+		/* only needed if we're switching chanctx (i.e. during CSA) */
+		if (switching_chanctx) {
 			mvmvif->ap_ibss_active = true;
 			break;
 		}
@@ -2858,12 +2858,12 @@ static int __iwl_mvm_assign_vif_chanctx(struct iwl_mvm *mvm,
 
 	/* Handle binding during CSA */
 	if ((vif->type == NL80211_IFTYPE_AP) ||
-	    (vif->csa_active && (vif->type == NL80211_IFTYPE_STATION))) {
+	    (switching_chanctx && (vif->type == NL80211_IFTYPE_STATION))) {
 		iwl_mvm_update_quotas(mvm, NULL);
 		iwl_mvm_mac_ctxt_changed(mvm, vif, false, NULL);
 	}
 
-	if (vif->csa_active && vif->type == NL80211_IFTYPE_STATION) {
+	if (switching_chanctx && vif->type == NL80211_IFTYPE_STATION) {
 		struct iwl_mvm_sta *mvmsta;
 
 		mvmsta = iwl_mvm_sta_from_staid_protected(mvm,
@@ -2922,7 +2922,7 @@ static void __iwl_mvm_unassign_vif_chanctx(struct iwl_mvm *mvm,
 		break;
 	case NL80211_IFTYPE_AP:
 		/* This part is triggered only during CSA */
-		if (!vif->csa_active || !mvmvif->ap_ibss_active)
+		if (!switching_chanctx || !mvmvif->ap_ibss_active)
 			goto out;
 
 		mvmvif->csa_countdown = false;
@@ -2936,7 +2936,7 @@ static void __iwl_mvm_unassign_vif_chanctx(struct iwl_mvm *mvm,
 		mvmvif->ap_ibss_active = false;
 		break;
 	case NL80211_IFTYPE_STATION:
-		if (!vif->csa_active)
+		if (!switching_chanctx)
 			break;
 
 		disabled_vif = vif;
