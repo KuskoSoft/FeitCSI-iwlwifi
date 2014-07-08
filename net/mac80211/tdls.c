37e6eba4daf08723a40a4ff1a17ee1437f8f272d
Liad Kaufman <liad.kaufman@intel.com>
2014-07-08 11:31:36 +0300

    [MERGEUPSTREAM] mac80211: only stop vif queues when sending TDLS teardown packet
    There is no need to stop all of the HW queues when sending a
    TDLS teardown packet. Instead, this stops only the queues that
    are assigned to the vif that handles the TDLS teardown flow.
    
    Change-Id: I4be236a4ab3b922ef3d517756bef1bf1b42679fe
    Signed-off-by: Liad Kaufman <liad.kaufman@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/40336
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Tested-by: IWL Jenkins
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    
    x-iwlwifi-stack-dev: 0ba6aa1bbb405b50231527a1bed5dabfc96d7d1e

--- net/mac80211/tdls.c
+++ net/mac80211/tdls.c
@@ -497,9 +497,8 @@ ieee80211_tdls_mgmt_teardown(struct wiphy *wiphy, struct net_device *dev,
 	 * During teardown, we prevent direct transmissions by stopping the
 	 * queues and flushing all direct packets.
 	 */
-	ieee80211_stop_queues_by_reason(&local->hw,
-			IEEE80211_MAX_QUEUE_MAP,
-			IEEE80211_QUEUE_STOP_REASON_TDLS_TEARDOWN, false);
+	ieee80211_stop_vif_queues(local, sdata,
+				  IEEE80211_QUEUE_STOP_REASON_TDLS_TEARDOWN);
 	ieee80211_flush_queues(local, sdata);
 
 	ret = ieee80211_tdls_prep_mgmt_packet(wiphy, dev, peer, action_code,
@@ -520,9 +519,8 @@ ieee80211_tdls_mgmt_teardown(struct wiphy *wiphy, struct net_device *dev,
 		clear_sta_flag(sta, WLAN_STA_TDLS_PEER_AUTH);
 	rcu_read_unlock();
 
-	ieee80211_wake_queues_by_reason(&local->hw,
-			IEEE80211_MAX_QUEUE_MAP,
-			IEEE80211_QUEUE_STOP_REASON_TDLS_TEARDOWN, false);
+	ieee80211_wake_vif_queues(local, sdata,
+				  IEEE80211_QUEUE_STOP_REASON_TDLS_TEARDOWN);
 
 	return 0;
 }
