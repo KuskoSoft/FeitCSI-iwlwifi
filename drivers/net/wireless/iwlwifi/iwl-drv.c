fb0502ffda7f8bdaf1653ccda93fdf0979decac2
Johannes Berg <johannes.berg@intel.com>
2014-09-01 14:54:41 +0200

    iwlwifi: make U-APSD default configurable at compile time
    Introduce a new Kconfig symbol for the default U-APSD setting.
    
    Change-Id: I88803a700ead3d763dd8009f9bd68b38cac571dd
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/43277
    Tested-by: IWL Jenkins
    
    x-iwlwifi-stack-dev: 2097d0c6425b9e0253392f152ff7d1346b5e7df5

--- drivers/net/wireless/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/iwlwifi/iwl-drv.c
@@ -1509,7 +1509,9 @@ struct iwl_mod_params iwlwifi_mod_params = {
 	.bt_coex_active = true,
 	.power_level = IWL_POWER_INDEX_1,
 	.wd_disable = true,
+#ifndef CPTCFG_IWLWIFI_UAPSD
 	.uapsd_disable = true,
+#endif /* CPTCFG_IWLWIFI_UAPSD */
 	/* the rest are 0 by default */
 };
 IWL_EXPORT_SYMBOL(iwlwifi_mod_params);
@@ -1691,7 +1693,11 @@ MODULE_PARM_DESC(nvm_file, "NVM file name");
 
 module_param_named(uapsd_disable, iwlwifi_mod_params.uapsd_disable,
 		   bool, S_IRUGO);
+#ifdef CPTCFG_IWLWIFI_UAPSD
+MODULE_PARM_DESC(uapsd_disable, "disable U-APSD functionality (default: N)");
+#else
 MODULE_PARM_DESC(uapsd_disable, "disable U-APSD functionality (default: Y)");
+#endif
 
 /*
  * set bt_coex_active to true, uCode will do kill/defer
