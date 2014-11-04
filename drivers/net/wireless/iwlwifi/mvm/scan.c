ee74cb6f9445437960b0b27948f092d0731ad22b
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2014-11-04 15:54:11 +0200

    iwlwifi: mvm: abort scan upon RFKILL [BUGFIX]
    This code existed but not for all the different FW APIs
    we support.
    Fix this.
    
    Change-Id: I7b3c8f97b86a3dbb951e152925f918211e0fafa1
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    ---
    MWG100220843
    Reviewed-on: https://gerrit.rds.intel.com/r/46662
    Tested-by: IWL Jenkins
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: f9e57ced3b1986917c5b40c293abba161cacd430

--- drivers/net/wireless/iwlwifi/mvm/scan.c
+++ drivers/net/wireless/iwlwifi/mvm/scan.c
@@ -666,16 +666,6 @@ static int iwl_mvm_cancel_regular_scan(struct iwl_mvm *mvm)
 					       SCAN_COMPLETE_NOTIFICATION };
 	int ret;
 
-	if (mvm->scan_status == IWL_MVM_SCAN_NONE)
-		return 0;
-
-	if (iwl_mvm_is_radio_killed(mvm)) {
-		ieee80211_scan_completed(mvm->hw, true);
-		iwl_mvm_unref(mvm, IWL_MVM_REF_SCAN);
-		mvm->scan_status = IWL_MVM_SCAN_NONE;
-		return 0;
-	}
-
 	iwl_init_notification_wait(&mvm->notif_wait, &wait_scan_abort,
 				   scan_abort_notif,
 				   ARRAY_SIZE(scan_abort_notif),
@@ -1552,6 +1542,17 @@ int iwl_mvm_cancel_scan(struct iwl_mvm *mvm)
 	if (mvm->fw->ucode_capa.capa[0] & IWL_UCODE_TLV_CAPA_UMAC_SCAN)
 		return iwl_umac_scan_stop(mvm, IWL_UMAC_SCAN_UID_REG_SCAN,
 					  true);
+
+	if (mvm->scan_status == IWL_MVM_SCAN_NONE)
+		return 0;
+
+	if (iwl_mvm_is_radio_killed(mvm)) {
+		ieee80211_scan_completed(mvm->hw, true);
+		iwl_mvm_unref(mvm, IWL_MVM_REF_SCAN);
+		mvm->scan_status = IWL_MVM_SCAN_NONE;
+		return 0;
+	}
+
 	if (mvm->fw->ucode_capa.api[0] & IWL_UCODE_TLV_API_LMAC_SCAN)
 		return iwl_mvm_scan_offload_stop(mvm, true);
 	return iwl_mvm_cancel_regular_scan(mvm);
