fe3a30d085897c21e0fd4a096e6c1806b9b1eb93
Johannes Berg <johannes.berg@intel.com>
2014-11-18 22:49:20 +0100

    iwlwifi: move SDIO/IDI registrations under ifdef
    If neither SDIO nor IDI are compiled, then neither their init
    functions nor the static inlines are needed, so make them all
    optional by moving them under the existing ifdef.
    
    While at it, remove a few useless comments that show up odd
    when the ifdef removal happens.
    
    Change-Id: I4fec64c3eb17409ea00f10960d6df5ca8b7f3573
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/47452
    Tested-by: IWL Jenkins
    x-iwlwifi-stack-dev: b2e2b990430081935c2f5433c9047485236e152b

--- drivers/net/wireless/iwlwifi/iwl-trans.h
+++ drivers/net/wireless/iwlwifi/iwl-trans.h
@@ -1015,7 +1015,7 @@ static inline void iwl_pci_unregister_driver(void)
 
 #endif /* CPTCFG_IWLWIFI_PCIE */
 
-/* SDIO */
+#if defined(CPTCFG_IWLWIFI_IDI) || defined(CPTCFG_IWLWIFI_SDIO)
 #ifdef CPTCFG_IWLWIFI_SDIO
 int __must_check iwl_sdio_register_driver(void);
 void iwl_sdio_unregister_driver(void);
@@ -1024,11 +1024,9 @@ static inline int __must_check iwl_sdio_register_driver(void) { return 0; }
 static inline void iwl_sdio_unregister_driver(void) {}
 #endif /* CPTCFG_IWLWIFI_SDIO */
 
-/* IDI */
 static inline int __must_check iwl_idi_register_driver(void) { return 0; }
 static inline void iwl_idi_unregister_driver(void) {}
 
-#if defined(CPTCFG_IWLWIFI_IDI) || defined(CPTCFG_IWLWIFI_SDIO)
 int __must_check iwl_slv_register_drivers(void);
 void iwl_slv_unregister_drivers(void);
 #else
