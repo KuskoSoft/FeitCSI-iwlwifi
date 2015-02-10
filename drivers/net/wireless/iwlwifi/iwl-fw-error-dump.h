2fc30cbd97368fa7928cd4ed6cba8bd258d343b8
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2015-02-10 14:29:48 +0200

    iwlwifi: mvm: add trigger for firmware dump upon MLME failures
    This will allow to catch failures in MLME and get the
    firmware data when this happens.
    
    Change-Id: I9f9885136a2d474c47ae15d74af83ac03917eb67
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/52269
    Tested-by: IWL Jenkins
    x-iwlwifi-stack-dev: 942a75efd582e39bdd84ef2468e58eb466d99d02

--- drivers/net/wireless/iwlwifi/iwl-fw-error-dump.h
+++ drivers/net/wireless/iwlwifi/iwl-fw-error-dump.h
@@ -246,6 +246,7 @@ iwl_fw_error_next_data(struct iwl_fw_error_dump_data *data)
  * @FW_DBG_TRIGGER_CHANNEL_SWITCH: trigger log collection upon channel switch.
  * @FW_DBG_TRIGGER_FW_NOTIF: trigger log collection when the firmware sends a
  *	command response or a notification.
+ * @FW_DBG_TRIGGER_MLME: trigger log collection upon MLME event.
  */
 enum iwl_fw_dbg_trigger {
 	FW_DBG_TRIGGER_INVALID = 0,
@@ -254,6 +255,7 @@ enum iwl_fw_dbg_trigger {
 	FW_DBG_TRIGGER_MISSED_BEACONS,
 	FW_DBG_TRIGGER_CHANNEL_SWITCH,
 	FW_DBG_TRIGGER_FW_NOTIF,
+	FW_DBG_TRIGGER_MLME,
 
 	/* must be last */
 	FW_DBG_TRIGGER_MAX,
