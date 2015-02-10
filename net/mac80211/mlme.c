6982caeec13303a4606e388a942c2daa7625ec10
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2015-02-10 13:11:01 +0200

    mac80211: convert rssi_callback() to event_callback()
    We will be able to add more events, such as MLME events and
    others. The low level driver may be interested in knowing
    about these events to dump firmware data upon failures, or
    to change parameters in case connection attempts fail etc...
    
    Change-Id: I1e7cf595d76c78b786f85ca394218923bf03dbc6
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/52256
    Tested-by: IWL Jenkins
    x-iwlwifi-stack-dev: c77bca17142eb25d186d55b46b90c6d57ca715a1

--- net/mac80211/mlme.c
+++ net/mac80211/mlme.c
@@ -3311,12 +3311,14 @@ static void ieee80211_rx_mgmt_beacon(struct ieee80211_sub_if_data *sdata,
 		if (sig > ifmgd->rssi_max_thold &&
 		    (last_sig <= ifmgd->rssi_min_thold || last_sig == 0)) {
 			ifmgd->last_ave_beacon_signal = sig;
-			drv_rssi_callback(local, sdata, RSSI_EVENT_HIGH);
+			drv_event_callback(local, sdata, RSSI_EVENT,
+					   RSSI_EVENT_HIGH);
 		} else if (sig < ifmgd->rssi_min_thold &&
 			   (last_sig >= ifmgd->rssi_max_thold ||
 			   last_sig == 0)) {
 			ifmgd->last_ave_beacon_signal = sig;
-			drv_rssi_callback(local, sdata, RSSI_EVENT_LOW);
+			drv_event_callback(local, sdata, RSSI_EVENT,
+					   RSSI_EVENT_LOW);
 		}
 	}
 
