5e102243486296bfd656a908ced484283b95a2ce
Johannes Berg <johannes.berg@intel.com>
2015-02-25 07:58:34 +0100

    iwlwifi: mvm: print message for U-APSD non-agg detection
    Otherwise we'll have to guess what caused disconnects.
    
    Change-Id: Ic2f23e887c2657a26d4ddce0124b6277020f093c
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/53012
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Tested-by: IWL Jenkins
    x-iwlwifi-stack-dev: 0e01e1fca93152c7a8222074d26eed48d996c2b8

--- drivers/net/wireless/iwlwifi/mvm/utils.c
+++ drivers/net/wireless/iwlwifi/mvm/utils.c
@@ -1060,6 +1060,8 @@ static void iwl_mvm_uapsd_agg_disconnect_iter(void *data, u8 *mac,
 		return;
 
 	mvm->tcm.data[*mac_id].uapsd_nonagg_detect.detected = true;
+	IWL_INFO(mvm,
+		 "detected AP should do aggregation but isn't, likely due to U-APSD\n");
 	schedule_delayed_work(&mvmvif->uapsd_nonagg_detected_wk, 5 * HZ);
 }
 
