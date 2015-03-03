0ff574cdbedf188d3f4b6a4e5207ef88dabfa63c
Max Stepanov <Max.Stepanov@intel.com>
2015-03-03 16:28:15 +0200

    iwlwifi: 8000: add PCI ID for 8260
    Add PCI ID entry for 8260: dev=0x24F3, subdev=0xC010.
    
    Change-Id: Ifcd7ba4beb87c292658576043bd1cfa04a72517c
    Signed-off-by: Max Stepanov <Max.Stepanov@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/53347
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    Tested-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: 7d903a50869aff955c4d828815c037d444f0668b

--- drivers/net/wireless/iwlwifi/pcie/drv.c
+++ drivers/net/wireless/iwlwifi/pcie/drv.c
@@ -226,6 +226,7 @@ static const struct pci_device_id iwl_hw_card_ids[] = {
 /* 8000 Series */
 	{IWL_PCI_DEVICE(0x24F3, 0x0000, iwl8260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24F3, 0x0010, iwl8260_2ac_cfg)},
+	{IWL_PCI_DEVICE(0x24F3, 0xC010, iwl8260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24F3, 0x0004, iwl8260_2n_cfg)},
 	{IWL_PCI_DEVICE(0x24F4, 0x0030, iwl8260_2ac_cfg)},
 	{IWL_PCI_DEVICE(0x24F5, 0x0010, iwl4165_2ac_cfg)},
