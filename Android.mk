958178f2b54655b0bbbd139a5906e93e82e8c68e
Benzi Zbit <benzi.zbit@intel.com>
2015-04-14 10:52:26 +0300

    Android.mk: Remove Apache license header
    Signed-off-by: Benzi Zbit <benzi.zbit@intel.com>
    Change-Id: I30b3956f04f006fd54280f2e3abc05e88be86ff7
    Reviewed-on: https://gerrit.rds.intel.com/r/55364
    Tested-by: IWL Jenkins
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    x-iwlwifi-stack-dev: 0f6b8b3c22ffc2b570efc89aa8673e0fc2cd3aea

--- Android.mk
+++ Android.mk
@@ -1,13 +1,3 @@
-#
-# Copyright (C) - Intel
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-
 # for integrated compat build
 # ** The below two lines is OTC tree specific & this will be addressed/removed shortly **
 COMPAT_KERNEL_MODULES += external/intel_iwlwifi
