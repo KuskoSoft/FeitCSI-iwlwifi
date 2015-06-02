abc41b1d19a0978ebcdeae726dde563011f51f5c
Gregory Greenman <gregory.greenman@intel.com>
2015-06-02 18:06:16 +0300

    iwlwifi: mvm: add basic Time of Flight (802.11mc FTM) support
    ToF is a time based method for measurement of the WiFi device
    location within a WiFi environment. The driver functionality provided
    by this patch is the interface for communication with FW and receiving
    location related updates from the FW. The interface provided by this
    patch is via debugfs.
    
    type=feature
    
    Change-Id: Id967d5c29771ab5aec2e5781130a21aa338f0bcc
    Signed-off-by: Gregory Greenman <gregory.greenman@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/58086
    Tested-by: IWL Jenkins
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: 9cff3ee6f6b8ab46b9ec40acc6292267ef26d7f1

--- drivers/net/wireless/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/iwlwifi/mvm/mvm.h
@@ -81,6 +81,7 @@
 #include "fw-api.h"
 #include "constants.h"
 #include "vendor-cmd.h"
+#include "tof.h"
 
 #ifdef CPTCFG_IWLWIFI_LTE_COEX
 #include "lte-coex.h"
@@ -764,6 +765,7 @@ struct iwl_mvm {
 
 	struct iwl_mvm_frame_stats drv_rx_stats;
 	spinlock_t drv_stats_lock;
+	struct iwl_mvm_tof_data tof_data;
 	u16 dbgfs_rx_phyinfo;
 #endif
 
