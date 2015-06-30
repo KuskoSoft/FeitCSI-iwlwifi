b0d32d9bba0f85612019eec4f1f026533497b932
Johannes Berg <johannes.berg@intel.com>
2015-06-30 15:31:22 +0200

    iwlwifi: mvm: move existing UMAC commands to group 1
    Existing UMAC commands already use the long header, but are sent
    with group 0 and the long header inserted manually. Move them to
    the group 1 to take advantage of the header building in the low-
    level transport.
    
    Existing firmware ignores the group_id field (it's reserved) and
    the first firmware that really supports long command headers can
    parse all commands in both group 0 (with short header) and group
    1 (with long header.)
    
    type=cleanup
    
    Change-Id: Ie49835f8054c8bafee641815cd8aef3c214d786b
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerrit.rds.intel.com/r/59351
    Tested-by: IWL Jenkins
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    x-iwlwifi-stack-dev: 6b02d9cb4ce60be6474468b888cee0bf0440802d

--- drivers/net/wireless/iwlwifi/trans_slave/shared.c
+++ drivers/net/wireless/iwlwifi/trans_slave/shared.c
@@ -1376,7 +1376,8 @@ static int iwl_slv_tx_enqueue_hcmd(struct iwl_trans *trans,
 	BUG_ON(!trans_slv->txqs);
 	BUG_ON(!cmd);
 
-	if (WARN(!trans_slv->wide_cmd_header && group_id != 0,
+	if (WARN(!trans_slv->wide_cmd_header &&
+		 group_id > IWL_ALWAYS_LONG_GROUP,
 		 "unsupported wide command %#x\n", cmd->id))
 		return -EINVAL;
 
