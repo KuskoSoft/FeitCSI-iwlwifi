e3a8d004290d534ed3caf8f096a07bf514cf5c0c
Luciano Coelho <luciano.coelho@intel.com>
2015-09-15 22:39:09 +0300

    [NOUPSTREAM] iwlwifi: pcie: allow runtime PM with the interface down
    Add a Kconfig flag to allow the device to go into runtime suspend when
    the interface is off.  This is under a Kconfig flag for now, because
    we can't differentiate platforms that want this (e.g. Broxton) from
    those which don't (e.g. Cherrytrail).  This will later be changed,
    when code to identify when this is needed becomes available.
    
    Basically what this patch does is to implement simple runtime
    suspend/resume functions and enable RTPM for the device.  Since, at
    this point, we don't have full RTPM support, we grab an RTPM reference
    to prevent runtime suspend when the interface is up.
    
    type=feature
    
    Change-Id: Ifeeca04035b4eb372ff8eda98f31479bd39a9d59
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/7196
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Grumbach, Emmanuel <emmanuel.grumbach@intel.com>
    x-iwlwifi-stack-dev: 09fc2f32991924a657341f72c3a3ae991e0e4f51

--- drivers/net/wireless/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/iwlwifi/pcie/trans.c
@@ -70,6 +70,9 @@
 #include <linux/bitops.h>
 #include <linux/gfp.h>
 #include <linux/vmalloc.h>
+#ifdef CPTCFG_IWLWIFI_RTPM_WIFI_OFF
+#include <linux/pm_runtime.h>
+#endif /* CPTCFG_IWLWIFI_RTPM_WIFI_OFF */
 
 #include "iwl-drv.h"
 #include "iwl-trans.h"
@@ -1298,6 +1301,9 @@ static void _iwl_trans_pcie_stop_device(struct iwl_trans *trans, bool low_power)
 	if (hw_rfkill != was_hw_rfkill)
 		iwl_trans_pcie_rf_kill(trans, hw_rfkill);
 
+#ifdef CPTCFG_IWLWIFI_RTPM_WIFI_OFF
+	pm_runtime_put_sync(trans->dev);
+#endif /* CPTCFG_IWLWIFI_RTPM_WIFI_OFF */
 #ifdef CPTCFG_IWLWIFI_PLATFORM_DATA
 	if (low_power && !iwl_trans_pcie_power_device_off(trans_pcie)) {
 		/* card is off, no need to re-take ownership */
@@ -1474,6 +1480,9 @@ static int _iwl_trans_pcie_start_hw(struct iwl_trans *trans, bool low_power)
 	/* ... rfkill can call stop_device and set it false if needed */
 	iwl_trans_pcie_rf_kill(trans, hw_rfkill);
 
+#ifdef CPTCFG_IWLWIFI_RTPM_WIFI_OFF
+	pm_runtime_get_sync(trans->dev);
+#endif /* CPTCFG_IWLWIFI_RTPM_WIFI_OFF */
 	return 0;
 }
 
@@ -1595,6 +1604,10 @@ void iwl_trans_pcie_free(struct iwl_trans *trans)
 	struct iwl_trans_pcie *trans_pcie = IWL_TRANS_GET_PCIE_TRANS(trans);
 	int i;
 
+#ifdef CPTCFG_IWLWIFI_RTPM_WIFI_OFF
+	/* TODO: check if this is really needed */
+	pm_runtime_disable(trans->dev);
+#endif /* CPTCFG_IWLWIFI_RTPM_WIFI_OFF */
 #ifdef CPTCFG_IWLWIFI_PLATFORM_DATA
 	/* Make sure the device is on before calling pci functions again.
 	 * This also ensures that the saved_state structure is freed.
@@ -1970,6 +1983,9 @@ void iwl_trans_pcie_ref(struct iwl_trans *trans)
 	    trans->dbg_cfg.wakelock_mode == IWL_WAKELOCK_MODE_IDLE)
 		wake_lock(&trans_pcie->ref_wake_lock);
 #endif
+#ifdef CPTCFG_IWLWIFI_RTPM_WIFI_OFF
+	pm_runtime_get(&trans_pcie->pci_dev->dev);
+#endif /* CPTCFG_IWLWIFI_RTPM_WIFI_OFF */
 	spin_unlock_irqrestore(&trans_pcie->ref_lock, flags);
 }
 
@@ -2001,6 +2017,10 @@ void iwl_trans_pcie_unref(struct iwl_trans *trans)
 				  msecs_to_jiffies(IWL_WAKELOCK_TIMEOUT_MS));
 	}
 #endif
+#ifdef CPTCFG_IWLWIFI_RTPM_WIFI_OFF
+	pm_runtime_mark_last_busy(&trans_pcie->pci_dev->dev);
+	pm_runtime_put_autosuspend(&trans_pcie->pci_dev->dev);
+#endif /* CPTCFG_IWLWIFI_RTPM_WIFI_OFF */
 
 	spin_unlock_irqrestore(&trans_pcie->ref_lock, flags);
 }
