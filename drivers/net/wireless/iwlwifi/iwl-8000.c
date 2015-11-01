177065b26cf72d216066c92e3b00d65cfae38a7e
Oren Givon <oren.givon@intel.com>
2015-11-01 15:27:58 +0200

    iwlwifi: Add PCI IDs for the new series 8165
    Add a new struct for the 8165 series and a few new
    PCI ID entries.
    
    type=feature
    
    Change-Id: Iae6e7a30e862fd30034790ed579262048d6d910f
    Signed-off-by: Oren Givon <oren.givon@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/7869
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Berg, Johannes <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: 2e93726dc5a95ea9ef5fcbd02163dae029340bf2

--- drivers/net/wireless/iwlwifi/iwl-8000.c
+++ drivers/net/wireless/iwlwifi/iwl-8000.c
@@ -190,6 +190,20 @@ const struct iwl_cfg iwl8260_2ac_cfg = {
 	.max_ht_ampdu_exponent = IEEE80211_HT_MAX_AMPDU_64K,
 };
 
+const struct iwl_cfg iwl8265_2ac_cfg = {
+	.name = "Intel(R) Dual Band Wireless AC 8265",
+	.fw_name_pre = IWL8000_FW_PRE,
+	IWL_DEVICE_8000,
+	.ht_params = &iwl8000_ht_params,
+	.nvm_ver = IWL8000_NVM_VERSION,
+	.nvm_calib_ver = IWL8000_TX_POWER_VERSION,
+#ifdef CPTCFG_IWLWIFI_SUPPORT_FPGA_BU
+	/* No need to load INIT image on FPGA */
+	.no_power_up_nic_in_init = true,
+#endif
+	.max_ht_ampdu_exponent = IEEE80211_HT_MAX_AMPDU_64K,
+};
+
 const struct iwl_cfg iwl4165_2ac_cfg = {
 	.name = "Intel(R) Dual Band Wireless AC 4165",
 	.fw_name_pre = IWL8000_FW_PRE,
