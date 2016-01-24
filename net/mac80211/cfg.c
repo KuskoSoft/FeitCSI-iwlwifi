7bcbe99b1e8c5ae51482c5a6ad07341e8ef4780b
Haim Dreyfuss <haim.dreyfuss@intel.com>
2016-01-24 11:40:19 +0200

    [BUGFIX]mac80211: Check ieee80211_vif_to_dev to avoid NULL dereferencing
    ieee80211_vif_to_dev may return NULL value.
    This might happen in two possible cases:
    1. The virtual interface in mac80211 isn't running yet.
    2. The interface wasn't added to the driver yet.
    Check it before using it's return value
    Note: this detected by Klockwork static analayzer
    
    type=bugfix
    fixes=I76d69488d21b438304deb07b2019300072a4d952
    fixes=I77de614aac7eb9949da866c63cf54bf8fda937a2
    bug=not-tracked
    
    Change-Id: I7278e72ad94f08cbcb89bfb6858e56aa4a862564
    Signed-off-by: Haim Dreyfuss <haim.dreyfuss@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/17255
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Grumbach, Emmanuel <emmanuel.grumbach@intel.com>
    Reviewed-by: Coelho, Luciano <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: e0fb952556f3a12ecca8d7b50158d5a9407061d1

--- net/mac80211/cfg.c
+++ net/mac80211/cfg.c
@@ -3593,6 +3593,7 @@ void ieee80211_nan_func_terminated(struct ieee80211_vif *vif,
 {
 	struct ieee80211_sub_if_data *sdata = vif_to_sdata(vif);
 	struct ieee80211_nan_func *func;
+	struct wireless_dev *wdev;
 	u64 cookie;
 
 	if (WARN_ON(vif->type != NL80211_IFTYPE_NAN))
@@ -3615,8 +3616,10 @@ void ieee80211_nan_func_terminated(struct ieee80211_vif *vif,
 	cfg80211_free_nan_func_members(&func->func);
 	kfree(func);
 
-	cfg80211_nan_func_terminated(ieee80211_vif_to_wdev(vif), inst_id,
-				     reason, cookie, gfp);
+	wdev = ieee80211_vif_to_wdev(vif);
+	if (!WARN_ON_ONCE(!wdev))
+		cfg80211_nan_func_terminated(wdev, inst_id,
+					     reason, cookie, gfp);
 }
 EXPORT_SYMBOL(ieee80211_nan_func_terminated);
 
@@ -3626,6 +3629,7 @@ void ieee80211_nan_func_match(struct ieee80211_vif *vif,
 {
 	struct ieee80211_sub_if_data *sdata = vif_to_sdata(vif);
 	struct ieee80211_nan_func *func;
+	struct wireless_dev *wdev;
 
 	if (WARN_ON(vif->type != NL80211_IFTYPE_NAN))
 		return;
@@ -3641,7 +3645,9 @@ void ieee80211_nan_func_match(struct ieee80211_vif *vif,
 
 	spin_unlock_bh(&sdata->u.nan.func_lock);
 
-	cfg80211_nan_match(ieee80211_vif_to_wdev(vif), match, gfp);
+	wdev = ieee80211_vif_to_wdev(vif);
+	if (!WARN_ON_ONCE(!wdev))
+		cfg80211_nan_match(wdev, match, gfp);
 }
 EXPORT_SYMBOL(ieee80211_nan_func_match);
 
