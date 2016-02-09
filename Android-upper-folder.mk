72aad7499c823a3e67e8f2668ff28cc551c6e5c1
Benzi Zbit <benzi.zbit@intel.com>
2016-02-09 15:12:13 +0200

    Android mk : Reorder android mk to better support 1A
    1 - Remove Android mk support for OC6 projects.
    2 - Consolidation of Android.mk and Android-make-iwlwifi.mk
        this will be done in two steps -
        Inorder to not brack the 1A build,
        we will first move all content to Android.mk,
        when this will be delivered remove the Android-make-iwlwifi.mk.
    3 - Add Android-upper-folder.mk to support 1A new folder structure.
    
    type=maint
    
    Change-Id: I93cd1a44997068dad24537fec13ab43edfe5c43a
    Signed-off-by: Benzi Zbit <benzi.zbit@intel.com>
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/21197
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 31e6e9ead5064c5b937b0597904eeccd833412b2

---
+++ Android-upper-folder.mk
@@ -0,0 +1,3 @@
+LOCAL_PATH := $(call my-dir)
+# include only the rigth Android.mk file.
+include $(LOCAL_PATH)/$(INTEL_IWL_MODULE_SUB_FOLDER)/Android.mk
--- Android.mk
+++ Android.mk
@@ -1,55 +1,82 @@
 # including iwlwifi Android mk only if it was declared.
 ifeq ($(BOARD_USING_INTEL_IWL),true)
 
-LOCAL_PATH := $(call my-dir)
+# Run only this build if variant define the needed configuration
+# e.g. Enabling iwlwifi for XMM6321
+# BOARD_USING_INTEL_IWL := true      - this will enable iwlwifi building
+# INTEL_IWL_BOARD_CONFIG := xmm6321  - the configuration, defconfig-xmm6321
+# INTEL_IWL_USE_COMPAT_INSTALL := y  - this will use kernel modules installation
+# INTEL_IWL_COMPAT_INSTALL_DIR := updates - the folder that the modules will be installed in
+# INTEL_IWL_COMPAT_INSTALL_PATH ?= $(ANDROID_BUILD_TOP)/$(TARGET_OUT) - the install path for the modules
+.PHONY: iwlwifi
 
-# Sofia projects
+INTEL_IWL_SRC_DIR := $(call my-dir)
+INTEL_IWL_OUT_DIR := $(abspath $(PRODUCT_OUT)/iwlwifi)
+INTEL_IWL_COMPAT_INSTALL_PATH ?= $(abspath $(TARGET_OUT))
 
-ifeq ($(INTEL_IWL_USE_SYSTEM_COMPAT_MOD_BUILD),y)
-# Build iwlwifi modules using system compat install
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := IWLWIFI
-LOCAL_KERNEL_COMPAT_DEFCONFIG := $(INTEL_IWL_BOARD_CONFIG)
-
-# $1: INSTALL_MOD_PATH
-# $2: Module source dir
-define COMPAT_PRIVATE_$(LOCAL_MODULE)_PREINSTALL
-        find $(1) -name modules.dep -exec cp {} $(2)/modules.dep.orig \;
-endef
-
-# $1: INSTALL_MOD_PATH
-# $2: Module source dir
-define COMPAT_PRIVATE_$(LOCAL_MODULE)_POSTINSTALL
-        find $(1) -path \*updates\*\.ko -type f -exec $(2)/intc-scripts/mv-compat-mod.py {} iwlmvm \;
-        find $(1) -name modules.dep -exec $(2)/intc-scripts/ren-compat-deps.py {} updates iwlmvm \;
-        find $(1) -name modules.dep -exec sh -c 'cat $(2)/modules.dep.orig >> {}' \;
-        find $(1) -name modules.alias -exec $(2)/intc-scripts/ren-compat-aliases.py {} iwlwifi \;
-endef
-
-include $(BUILD_COMPAT_MODULE)
-
-else
-
-# CHT51
-#
-# include only the rigth Android-make-iwlwifi.mk file.
-#
-# in the CHT51 tree we may have multiple iwlwifi in different folders, this will cause
-# that the only one that will be evaluated is the last one included.
-#
-# the preferred way to handle this is to include the right one from the BoardConfig.mk file,
-# but looks like some of the needed variables are not defined when this file is included, what will
-# cause a failure in the build. so we need to include the file later on, when all Android mk are
-# included and all variables are defined. so we do this from here.
-#
-# (for now) INTEL_IWL_MODULE_SUB_FOLDER can hold uefi/cht , sofia/lte , sofia/3gr
-
-ifeq ($(INTEL_IWL_MODULE_SUB_FOLDER),$(LOCAL_PATH))
-	include $(INTEL_IWL_MODULE_SUB_FOLDER)/Android-make-iwlwifi.mk
+ifeq ($(CROSS_COMPILE),)
+ifneq ($(TARGET_TOOLS_PREFIX),)
+CROSS_COMPILE := CROSS_COMPILE=$(notdir $(TARGET_TOOLS_PREFIX))
+endif
 endif
 
-endif # ifeq ($(INTEL_IWL_USE_SYSTEM_COMPAT_MOD_BUILD),y)
+ifeq ($(CROSS_COMPILE),)
+ifeq ($(TARGET_ARCH),arm)
+$(warning You are building for an ARM platform, but no CROSS_COMPILE is set. This is likely an error.)
+else
+$(warning CROSS_COMPILE variant is not set; Defaulting to host gcc.)
+endif
+endif
+
+ifeq ($(INTEL_IWL_USE_COMPAT_INSTALL),y)
+INTEL_IWL_COMPAT_INSTALL := iwlwifi_install
+INTEL_IWL_KERNEL_DEPEND := $(INSTALLED_KERNEL_TARGET)
+else
+# use system install
+copy_modules_to_root: iwlwifi
+ALL_KERNEL_MODULES += $(INTEL_IWL_OUT_DIR)
+INTEL_IWL_KERNEL_DEPEND := build_bzImage
+endif
+
+ifeq ($(INTEL_IWL_USE_RM_MAC_CFG),y)
+copy_modules_to_root: iwlwifi
+INTEL_IWL_COMPAT_INSTALL_PATH := $(abspath $(KERNEL_OUT_MODINSTALL))
+INTEL_IWL_KERNEL_DEPEND := modules_install
+INTEL_IWL_RM_MAC_CFG_DEPEND := iwlwifi_rm_mac_cfg
+INTEL_IWL_INSTALL_MOD_STRIP := INSTALL_MOD_STRIP=1
+endif
+
+# IWLWIFI_CONFIGURE is a rule to use a defconfig for iwlwifi
+IWLWIFI_CONFIGURE := $(INTEL_IWL_OUT_DIR)/.config
+
+KERNEL_OUT_ABS_DIR := $(abspath $(KERNEL_OUT_DIR))
+
+iwlwifi: iwlwifi_build $(INTEL_IWL_COMPAT_INSTALL) $(INTEL_IWL_MOD_DEP)
+
+INTEL_IWL_SRC_FILES := $(shell find $(INTEL_IWL_SRC_DIR) -path $(INTEL_IWL_SRC_DIR)/.git -prune -o -print)
+$(INTEL_IWL_OUT_DIR): $(INTEL_IWL_SRC_FILES)
+	@echo Syncing directory $(INTEL_IWL_SRC_DIR) to $(INTEL_IWL_OUT_DIR)
+	@rsync -rt --del $(INTEL_IWL_SRC_DIR)/ $(INTEL_IWL_OUT_DIR)
+
+$(IWLWIFI_CONFIGURE): $(INTEL_IWL_KERNEL_DEPEND) $(INTEL_IWL_OUT_DIR)
+	@echo Configuring kernel module iwlwifi with defconfig-$(INTEL_IWL_BOARD_CONFIG)
+	@$(MAKE) -C $(INTEL_IWL_OUT_DIR)/ ARCH=$(TARGET_ARCH) $(CROSS_COMPILE) KLIB_BUILD=$(KERNEL_OUT_ABS_DIR) defconfig-$(INTEL_IWL_BOARD_CONFIG)
+
+iwlwifi_build: $(IWLWIFI_CONFIGURE)
+	@$(info Building kernel module iwlwifi in $(INTEL_IWL_OUT_DIR))
+	@$(MAKE) -C $(INTEL_IWL_OUT_DIR)/ ARCH=$(TARGET_ARCH) $(CROSS_COMPILE) KLIB_BUILD=$(KERNEL_OUT_ABS_DIR)
+
+iwlwifi_install: iwlwifi_build $(INTEL_IWL_RM_MAC_CFG_DEPEND)
+	@$(info Installing kernel modules in $(INTEL_IWL_COMPAT_INSTALL_PATH))
+	@$(MAKE) -C $(KERNEL_OUT_ABS_DIR) ARCH=$(TARGET_ARCH) M=$(INTEL_IWL_OUT_DIR)/ INSTALL_MOD_DIR=$(INTEL_IWL_COMPAT_INSTALL_DIR) INSTALL_MOD_PATH=$(INTEL_IWL_COMPAT_INSTALL_PATH) $(INTEL_IWL_INSTALL_MOD_STRIP) modules_install
+
+iwlwifi_rm_mac_cfg: iwlwifi_build
+	$(info Remove kernel cfg80211.ko and mac80211.ko)
+	@find $(KERNEL_OUT_MODINSTALL)/lib/modules/ -name "mac80211.ko" | xargs rm -f
+	@find $(KERNEL_OUT_MODINSTALL)/lib/modules/ -name "cfg80211.ko" | xargs rm -f
+
+.PHONY: iwlwifi_clean
+iwlwifi_clean:
+	rm -rf $(INTEL_IWL_OUT_DIR)
 
 endif
