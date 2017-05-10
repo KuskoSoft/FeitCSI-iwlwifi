33e2da3369d3e5f15cfb726691efb1e71207b099
Liad Kaufman <liad.kaufman@intel.com>
2017-05-10 15:48:05 +0300

    [NOUPSTREAM] iwlwifi: support HE PPE value override
    For debug purposes, support overriding the default
    PPE Threshold values, and on the way update their
    default values.
    
    type=feature
    
    Change-Id: I7708b80a3ff5302b5160a13d1faf4dadf655cb52
    Signed-off-by: Liad Kaufman <liad.kaufman@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/115972
    Reviewed-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Coelho, Luciano <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: cf4bc5c9d18f9ca763b1b99ed8c90a1f4ba2ff0c

--- drivers/net/wireless/intel/iwlwifi/iwl-dbg-cfg.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-dbg-cfg.h
@@ -276,6 +276,7 @@ struct iwl_dbg_cfg {
 	IWL_MOD_PARAM(int, sw_crypto)
 	IWL_MOD_PARAM(uint, disable_11n)
 	IWL_MOD_PARAM(uint, d0i3_entry_delay)
+	IWL_DBG_CFG_BIN(he_ppe_thres)
 #ifdef CPTCFG_IWLMVM_WAKELOCK
 	IWL_DBG_CFG(u32, WAKELOCK_TIMEOUT_MS)
 #endif /* CPTCFG_IWLMVM_WAKELOCK */
