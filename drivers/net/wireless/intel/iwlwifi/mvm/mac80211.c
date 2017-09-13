7f712882668a469140c52554d689720350e3f644
Shaul Triebitz <shaul.triebitz@intel.com>
2017-09-13 16:46:14 +0300

    [BUGFIX] iwlwifi: mvm: do not print security error in monitor mode
    In monitor mode we are not expected to decrypt encrypted
    packets (not having the keys).
    Hence we are expected to get an unknown rx security status.
    Keeping the print in monitor mode causes a print for each
    captured packet flooding the dmesg.
    
    type=bugfix
    fixes=unknown
    ticket=jira:WIFILNX-1371
    
    Change-Id: Ie76d96aeb4c3572ce4587fab9ea23fad1d769526
    Signed-off-by: Shaul Triebitz <shaul.triebitz@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/138005
    Reviewed-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Coelho, Luciano <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: 096b185d9cd5442e0769526cd99dcd389cc8fa5f

--- drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
@@ -1208,6 +1208,7 @@ static void iwl_mvm_restart_cleanup(struct iwl_mvm *mvm)
 	mvm->vif_count = 0;
 	mvm->rx_ba_sessions = 0;
 	mvm->fwrt.dump.conf = FW_DBG_INVALID;
+	mvm->monitor_on = false;
 
 	/* keep statistics ticking */
 	iwl_mvm_accu_radio_stats(mvm);
@@ -1636,6 +1637,9 @@ static int iwl_mvm_mac_add_interface(struct ieee80211_hw *hw,
 	iwl_mvm_tcm_add_vif(mvm, vif);
 #endif
 
+	if (vif->type == NL80211_IFTYPE_MONITOR)
+		mvm->monitor_on = true;
+
 	iwl_mvm_vif_dbgfs_register(mvm, vif);
 	goto out_unlock;
 
@@ -1747,6 +1751,9 @@ static void iwl_mvm_mac_remove_interface(struct ieee80211_hw *hw,
 
 	iwl_mvm_power_update_mac(mvm);
 	iwl_mvm_mac_ctxt_remove(mvm, vif);
+
+	if (vif->type == NL80211_IFTYPE_MONITOR)
+		mvm->monitor_on = false;
 
 #ifdef CPTCFG_IWLMVM_TDLS_PEER_CACHE
 	iwl_mvm_tdls_peer_cache_clear(mvm, vif);
