c92bbcf6a279586780e1acfd23e762dabb2273d6
Luca Coelho <luciano.coelho@intel.com>
2018-06-20 12:31:31 +0300

    Merge remote-tracking branch 'auto/master' into HEAD
    type=maint
    ticket=none
    
    Change-Id: I05268566cb0e2f4319d51fedd57585062e8cfb07
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: e7b9f42a846d068a82263ca8dd7f5d60fb37e656

--- backport-include/linux/string.h
+++ backport-include/linux/string.h
@@ -29,4 +29,8 @@ void memzero_explicit(void *s, size_t count);
 ssize_t strscpy(char *dest, const char *src, size_t count);
 #endif
 
+#if LINUX_VERSION_IS_LESS(4,6,0)
+int match_string(const char * const *array, size_t n, const char *string);
+#endif /* LINUX_VERSION_IS_LESS(4,5,0) */
+
 #endif /* __BACKPORT_LINUX_STRING_H */
