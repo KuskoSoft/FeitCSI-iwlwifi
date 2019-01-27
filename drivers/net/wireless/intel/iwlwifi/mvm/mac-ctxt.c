1fe95fec536b9210954424ed9f5764d8c9f86a94
Sara Sharon <sara.sharon@intel.com>
2019-01-27 11:24:30 +0200

    iwlwifi: mvm: reject new beacons when in inject mode
    Verify we do not accept new beacon templates while beacon
    injection is active.
    
    type=feature
    ticket=none
    
    Change-Id: I34654f005627a1f8fb02ad7efcbdc2962785ca05
    Signed-off-by: Sara Sharon <sara.sharon@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/206923
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Coelho, Luciano <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: c0d96d122b3996fbd29f96be04ca83cc9167b0b8

--- drivers/net/wireless/intel/iwlwifi/mvm/mac-ctxt.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mac-ctxt.c
@@ -1061,6 +1061,11 @@ int iwl_mvm_mac_ctxt_beacon_changed(struct iwl_mvm *mvm,
 	if (!beacon)
 		return -ENOMEM;
 
+#ifdef CPTCFG_IWLWIFI_DEBUGFS
+	if (mvm->beacon_inject_active)
+		return -EBUSY;
+#endif
+
 	ret = iwl_mvm_mac_ctxt_send_beacon(mvm, vif, beacon);
 	dev_kfree_skb(beacon);
 	return ret;
