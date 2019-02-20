696fa84f8ecdfc9dc46ae493231bef59ab7bf835
Luca Coelho <luciano.coelho@intel.com>
2019-02-20 10:38:06 +0200

    [BUGFIX] iwlwifi: mvm: define beacon_inject_active also when CONFIG_PM is disabled
    The beacon_inject_active element doesn't have anything to do with PM,
    but was accidentally defined inside a block that is #ifdef'ed on
    CONFIG_PM.  This causes compilation errors with configurations that
    don't have PM enabled.  Move it outside the CONFIG_PM ifdef.
    
    type=bugfix
    fixes=I34654f005627a1f8fb02ad7efcbdc2962785ca05
    ticket=none
    
    Change-Id: I7f38db25a7b47ecde4d3b0cf7242d22bb5c27fa8
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/209960
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 5969353a0a1363ab1efa4e4dbd81ba5fe08b0e89

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -1042,6 +1042,7 @@ struct iwl_mvm {
 	u32 dbgfs_prph_reg_addr;
 	bool disable_power_off;
 	bool disable_power_off_d3;
+	bool beacon_inject_active;
 
 	bool scan_iter_notif_enabled;
 
@@ -1104,7 +1105,6 @@ struct iwl_mvm {
 	u32 d3_test_pme_ptr;
 	struct ieee80211_vif *keep_vif;
 	u32 last_netdetect_scans; /* no. of scans in the last net-detect wake */
-	bool beacon_inject_active;
 #endif
 #endif
 
