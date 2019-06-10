9cdc2e1fa908e20aa5571823ab42c2eb3d115712
Alex Malamud <alex.malamud@intel.com>
2019-06-10 15:19:23 +0300

    iwlwifi: Set w-pointer upon resume according to SN
    During D3 state, FW may send packets.
    As a result, "write" queue pointer will be incremented by FW.
    Upon resume from D3, driver should adjust its shadows of "write" and "read"
    pointers to the value reported by FW.
    
    1. Keep TID used during wowlan configuration.
    2. Upon resume, set driver's "write" and "read" queue pointers
    	to the value reported by FW.
    
    type=feature
    ticket=jira:WIFI-25373
    
    Change-Id: Ib84f68bbef7fbf67ccf3c8a226811015c0ad6cda
    Signed-off-by: Alex Malamud <alex.malamud@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/225256
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: 729f30b21804d426320d368ffb1386cc1030745d

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -1062,6 +1062,7 @@ struct iwl_mvm {
 	struct ieee80211_channel **nd_channels;
 	int n_nd_channels;
 	bool net_detect;
+	u8 offload_tid;
 #ifdef CPTCFG_IWLWIFI_DEBUGFS
 	bool d3_wake_sysassert;
 	bool d3_test_active;
