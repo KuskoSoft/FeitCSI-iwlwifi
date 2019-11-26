2972b1d8f0e8add472b9e8228a029d8728f02be8
Avraham Stern <avraham.stern@intel.com>
2019-11-26 10:52:08 +0200

    [BUGFIX] cfg80211: avoid warning when interface is brought down during scan
    When an interface is brought down (e.g. because of rfkill) while
    scan is in progress, the scan request is cancelled and cfg80211
    expects the driver to notify that the scan is done.
    
    On devices that set the WIPHY_FLAG_SPLIT_SCAN_6GHZ flag,
    the scan request submitted to the driver uses the rdev->int_scan_req
    strcut instead of the rdev->scan_req struct. However, cfg80211 checks
    the rdev->scan_req struct to see if it was notified as done, and this
    results in a warning.
    
    Fix it by not warning if the rdev->int_scan_req was notified instead
    of rdev->scan_req.
    
    type=bugfix
    ticket=jira:WIFI-33127
    fixes=Icb3cbe6ba1969223fb1258b8bb9246960a1de617
    
    Change-Id: I6238537861383db0790eddd79e1022b5cff575d0
    Signed-off-by: Avraham Stern <avraham.stern@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/244784
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: 46903028af023a18933c9c005c09404c8affbf68

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -1357,7 +1357,9 @@ static int cfg80211_netdev_notifier_call(struct notifier_block *nb,
 	case NETDEV_DOWN:
 		cfg80211_update_iface_num(rdev, wdev->iftype, -1);
 		if (rdev->scan_req && rdev->scan_req->wdev == wdev) {
-			if (WARN_ON(!rdev->scan_req->notified))
+			if (WARN_ON(!rdev->scan_req->notified &&
+				    (!rdev->int_scan_req ||
+				     !rdev->int_scan_req->notified)))
 				rdev->scan_req->info.aborted = true;
 			___cfg80211_scan_done(rdev, false);
 		}
