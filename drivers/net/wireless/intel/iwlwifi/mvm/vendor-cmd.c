59996cd925355b1cf2bb89f516b79334f9be1c92
Johannes Berg <johannes.berg@intel.com>
2020-03-26 11:34:49 +0100

    [NOUPSTREAM] iwlwifi: mvm: FIPS test commands don't depend on ACPI
    There's no reason to have the FIPS test vendor commands depend on
    CONFIG_ACPI, looks like this was just misplaced. Fix it.
    
    type=cleanup
    ticket=none
    
    Change-Id: Ia4a104698fb73cbeea5730b04b7284f2ba2d6692
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/256985
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Luciano Coelho <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: 52bb8b10f391a6470d3c72f4a01f4fa0fa4ff411

--- drivers/net/wireless/intel/iwlwifi/mvm/vendor-cmd.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/vendor-cmd.c
@@ -8,7 +8,7 @@
  * Copyright(c) 2012 - 2014 Intel Corporation. All rights reserved.
  * Copyright(c) 2013 - 2015 Intel Mobile Communications GmbH
  * Copyright(c) 2016 - 2017 Intel Deutschland GmbH
- * Copyright (C) 2018 - 2019 Intel Corporation
+ * Copyright (C) 2018 - 2020 Intel Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of version 2 of the GNU General Public License as
@@ -31,7 +31,7 @@
  * Copyright(c) 2012 - 2014 Intel Corporation. All rights reserved.
  * Copyright(c) 2013 - 2015 Intel Mobile Communications GmbH
  * Copyright(c) 2016 - 2017 Intel Deutschland GmbH
- * Copyright (C) 2018 - 2019 Intel Corporation
+ * Copyright (C) 2018 - 2020 Intel Corporation
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -941,6 +941,7 @@ out:
 
 	return cfg80211_vendor_cmd_reply(skb);
 }
+#endif
 
 static const struct nla_policy
 iwl_mvm_vendor_fips_hw_policy[NUM_IWL_VENDOR_FIPS_TEST_VECTOR_HW] = {
@@ -1174,7 +1175,6 @@ free:
 	kfree(tb);
 	return ret;
 }
-#endif
 
 static int iwl_mvm_vendor_csi_register(struct wiphy *wiphy,
 				       struct wireless_dev *wdev,
@@ -1358,6 +1358,7 @@ static const struct wiphy_vendor_command iwl_mvm_vendor_commands[] = {
 		.policy = iwl_mvm_vendor_attr_policy,
 		.maxattr = MAX_IWL_MVM_VENDOR_ATTR,
 	},
+#endif
 	{
 		.info = {
 			.vendor_id = INTEL_OUI,
@@ -1369,7 +1370,6 @@ static const struct wiphy_vendor_command iwl_mvm_vendor_commands[] = {
 		.policy = iwl_mvm_vendor_fips_hw_policy,
 		.maxattr = MAX_IWL_VENDOR_FIPS_TEST_VECTOR_HW,
 	},
-#endif
 	{
 		.info = {
 			.vendor_id = INTEL_OUI,
