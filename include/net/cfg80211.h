9dccb1a14e68e73a2d910ab3c232100304bfe04b
Ilan Peer <ilan.peer@intel.com>
2020-05-20 14:31:16 +0300

    cfg80211: Parse SAE H2E only membership selector
    This extends the support for drivers that rebuild IEs in the
    FW (same as with HT/VHT/HE).
    
    type=feature
    ticket=none
    
    Change-Id: I1a93ae71c57ef0b6f58f99d47fce919d19d65ff0
    Signed-off-by: Ilan Peer <ilan.peer@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/263318
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Luciano Coelho <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: e6bc6e43dd15827ef6eb5feb8816387e45da9a79

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -1015,6 +1015,7 @@ enum cfg80211_ap_settings_flags {
  * @ht_required: stations must support HT
  * @vht_required: stations must support VHT
  * @he_required: stations must support HE
+ * @sae_h2e_required: stations must support direct H2E technique in SAE
  * @flags: flags, as defined in enum cfg80211_ap_settings_flags
  * @he_oper: HE operation IE (or %NULL if HE isn't enabled)
  * @twt_responder: Enable Target Wait Time
@@ -1045,7 +1046,7 @@ struct cfg80211_ap_settings {
 	const struct ieee80211_vht_cap *vht_cap;
 	const struct ieee80211_he_cap_elem *he_cap;
 	const struct ieee80211_he_operation *he_oper;
-	bool ht_required, vht_required, he_required;
+	bool ht_required, vht_required, he_required, sae_h2e_required;
 	bool twt_responder;
 	u32 flags;
 	struct ieee80211_he_obss_pd he_obss_pd;
