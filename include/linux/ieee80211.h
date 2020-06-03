torvalds/linux

commit 1b3a2e494bc793445f576c5476e9767cf7621684

mac80211: handle extended channel switch announcement
Handle the (public) extended channel switch announcement
action frames. Parts of the data in these frames isn't
really in IEs, but put it into the elems struct anyway
to simplify the handling.

Signed-off-by: Johannes Berg <johannes.berg@intel.com>

include/linux/ieee80211.h
@@ -866,6 +866,11 @@ struct ieee80211_mgmt {
 				} __packed chan_switch;
 				struct{
 					u8 action_code;
+					struct ieee80211_ext_chansw_ie data;
+					u8 variable[0];
+				} __packed ext_chan_switch;
+				struct{
+					u8 action_code;
 					u8 dialog_token;
 					u8 element_id;
 					u8 length;
@@ -1816,6 +1821,7 @@ enum ieee80211_key_len {
 
 /* Public action codes */
 enum ieee80211_pub_actioncode {
+	WLAN_PUB_ACTION_EXT_CHANSW_ANN = 4,
 	WLAN_PUB_ACTION_TDLS_DISCOVER_RES = 14,
 };
 

net/mac80211/mlme.c
@@ -3100,6 +3100,8 @@ void ieee80211_sta_rx_queued_mgmt(struct ieee80211_sub_if_data *sdata,
 	enum rx_mgmt_action rma = RX_MGMT_NONE;
 	u8 deauth_buf[IEEE80211_DEAUTH_FRAME_LEN];
 	u16 fc;
+	struct ieee802_11_elems elems;
+	int ies_len;
 
 	rx_status = (struct ieee80211_rx_status *) skb->cb;
 	mgmt = (struct ieee80211_mgmt *) skb->data;
@@ -3130,10 +3132,9 @@ void ieee80211_sta_rx_queued_mgmt(struct ieee80211_sub_if_data *sdata,
 		break;
 	case IEEE80211_STYPE_ACTION:
 		if (mgmt->u.action.category == WLAN_CATEGORY_SPECTRUM_MGMT) {
-			struct ieee802_11_elems elems;
-			int ies_len = skb->len -
-				      offsetof(struct ieee80211_mgmt,
-					       u.action.u.chan_switch.variable);
+			ies_len = skb->len -
+				  offsetof(struct ieee80211_mgmt,
+					   u.action.u.chan_switch.variable);
 
 			if (ies_len < 0)
 				break;
@@ -3144,6 +3145,28 @@ void ieee80211_sta_rx_queued_mgmt(struct ieee80211_sub_if_data *sdata,
 
 			if (elems.parse_error)
 				break;
+
+			ieee80211_sta_process_chanswitch(sdata,
+							 rx_status->mactime,
+							 &elems);
+		} else if (mgmt->u.action.category == WLAN_CATEGORY_PUBLIC) {
+			ies_len = skb->len -
+				  offsetof(struct ieee80211_mgmt,
+					   u.action.u.ext_chan_switch.variable);
+
+			if (ies_len < 0)
+				break;
+
+			ieee802_11_parse_elems(
+				mgmt->u.action.u.ext_chan_switch.variable,
+				ies_len, &elems);
+
+			if (elems.parse_error)
+				break;
+
+			/* for the handling code pretend this was also an IE */
+			elems.ext_chansw_ie =
+				&mgmt->u.action.u.ext_chan_switch.data;
 
 			ieee80211_sta_process_chanswitch(sdata,
 							 rx_status->mactime,

net/mac80211/rx.c
@@ -2424,6 +2424,22 @@ ieee80211_rx_h_action(struct ieee80211_rx_data *rx)
 		}
 
 		break;
+	case WLAN_CATEGORY_PUBLIC:
+		if (len < IEEE80211_MIN_ACTION_SIZE + 1)
+			goto invalid;
+		if (sdata->vif.type != NL80211_IFTYPE_STATION)
+			break;
+		if (!rx->sta)
+			break;
+		if (!ether_addr_equal(mgmt->bssid, sdata->u.mgd.bssid))
+			break;
+		if (mgmt->u.action.u.ext_chan_switch.action_code !=
+				WLAN_PUB_ACTION_EXT_CHANSW_ANN)
+			break;
+		if (len < offsetof(struct ieee80211_mgmt,
+				   u.action.u.ext_chan_switch.variable))
+			goto invalid;
+		goto queue;
 	case WLAN_CATEGORY_VHT:
 		if (sdata->vif.type != NL80211_IFTYPE_STATION &&
 		    sdata->vif.type != NL80211_IFTYPE_MESH_POINT &&

KuskoSoft/FeitCSI-iwlwifi

commit dbd98a891b21e5304b3631955d6afe198d441301

[BUGFIX] mac80211: drop protected action frames on MFP connections
While looking into some other issue, Jouni reported that we will
always treat public action frames, in particular extended channel
switch public action frames, regardless of whether the connection
is MFP or not. On an MFP connection, these should be dropped and
the protected dual be required instead.

Drop public action frames on an MFP connection, and also add the
necessary code to handle their protected duals, which look the
same apart from the category value.

type=bugfix
ticket=none
fixes=I16ddf8369a18ae8bf1f43fda86b64bce01bb3270

Change-Id: I4d4bb6e9e614465ee403b7d36d3f7e1dac810092
Reported-by: Jouni Malinen <j@w1.fi>
Fixes: 1b3a2e494bc7 ("mac80211: handle extended channel switch announcement")
Signed-off-by: Johannes Berg <johannes.berg@intel.com>
Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/263357
Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
Reviewed-by: Luciano Coelho <luciano.coelho@intel.com>
x-iwlwifi-stack-dev: 2fcf474b8f47557cffb0381355da8263ceab1cb7

include/linux/ieee80211.h
@@ -955,6 +955,8 @@ struct ieee80211_mgmt {
 		struct {
 			u8 category;
 			union {
+				/* all have the action_code first */
+				u8 action_code;
 				struct {
 					u8 action_code;
 					u8 dialog_token;
@@ -2660,7 +2662,7 @@ enum ieee80211_category {
 	WLAN_CATEGORY_RADIO_MEASUREMENT = 5,
 	WLAN_CATEGORY_HT = 7,
 	WLAN_CATEGORY_SA_QUERY = 8,
-	WLAN_CATEGORY_PROTECTED_DUAL_OF_ACTION = 9,
+	WLAN_CATEGORY_PROTECTED_DUAL_OF_PUBLIC_ACTION = 9,
 	WLAN_CATEGORY_WNM = 10,
 	WLAN_CATEGORY_WNM_UNPROTECTED = 11,
 	WLAN_CATEGORY_TDLS = 12,
@@ -3271,6 +3273,22 @@ static inline u8 *ieee80211_get_DA(struct ieee80211_hdr *hdr)
 		return hdr->addr3;
 	else
 		return hdr->addr1;
+}
+
+static inline bool ieee80211_public_action_has_protected_dual(u8 action)
+{
+	switch (action) {
+	case WLAN_PUB_ACTION_20_40_BSS_COEX:
+	case WLAN_PUB_ACTION_DSE_REG_LOC_ANN:
+	case WLAN_PUB_ACTION_MSMT_PILOT:
+	case WLAN_PUB_ACTION_TDLS_DISCOVER_RES:
+	case WLAN_PUB_ACTION_LOC_TRACK_NOTI:
+	case WLAN_PUB_ACTION_PUBLIC_KEY:
+		return false;
+	default:
+		/* safe default - yes, should be protected */
+		return true;
+	}
 }
 
 /**

net/mac80211/mlme.c
@@ -4318,7 +4318,8 @@ void ieee80211_sta_rx_queued_mgmt(struct ieee80211_sub_if_data *sdata,
 		ieee80211_rx_mgmt_assoc_resp(sdata, mgmt, skb->len);
 		break;
 	case IEEE80211_STYPE_ACTION:
-		if (mgmt->u.action.category == WLAN_CATEGORY_SPECTRUM_MGMT) {
+		switch (mgmt->u.action.category) {
+		case WLAN_CATEGORY_SPECTRUM_MGMT:
 			ies_len = skb->len -
 				  offsetof(struct ieee80211_mgmt,
 					   u.action.u.chan_switch.variable);
@@ -4338,7 +4339,9 @@ void ieee80211_sta_rx_queued_mgmt(struct ieee80211_sub_if_data *sdata,
 						 rx_status->mactime,
 						 rx_status->device_timestamp,
 						 &elems, false);
-		} else if (mgmt->u.action.category == WLAN_CATEGORY_PUBLIC) {
+			break;
+		case WLAN_CATEGORY_PUBLIC:
+		case WLAN_CATEGORY_PROTECTED_DUAL_OF_PUBLIC_ACTION:
 			ies_len = skb->len -
 				  offsetof(struct ieee80211_mgmt,
 					   u.action.u.ext_chan_switch.variable);
@@ -4365,6 +4368,7 @@ void ieee80211_sta_rx_queued_mgmt(struct ieee80211_sub_if_data *sdata,
 						 rx_status->mactime,
 						 rx_status->device_timestamp,
 						 &elems, false);
+			break;
 		}
 		break;
 	}

net/mac80211/rx.c
@@ -3153,6 +3153,7 @@ ieee80211_rx_h_action(struct ieee80211_rx_data *rx)
 	struct ieee80211_mgmt *mgmt = (struct ieee80211_mgmt *) rx->skb->data;
 	struct ieee80211_rx_status *status = IEEE80211_SKB_RXCB(rx->skb);
 	int len = rx->skb->len;
+	u8 action_code;
 
 	if (!ieee80211_is_action(mgmt->frame_control))
 		return RX_CONTINUE;
@@ -3269,6 +3270,16 @@ ieee80211_rx_h_action(struct ieee80211_rx_data *rx)
 
 		break;
 	case WLAN_CATEGORY_PUBLIC:
+		if (len < IEEE80211_MIN_ACTION_SIZE + 1)
+			goto invalid;
+		action_code = mgmt->u.action.u.action_code;
+		if (rx->sta && test_sta_flag(rx->sta, WLAN_STA_MFP) &&
+		    ieee80211_public_action_has_protected_dual(action_code)) {
+			/* FIXME: let userspace say it's OK with these */
+			goto invalid;
+		}
+		/* fall through */
+	case WLAN_CATEGORY_PROTECTED_DUAL_OF_PUBLIC_ACTION:
 		if (len < IEEE80211_MIN_ACTION_SIZE + 1)
 			goto invalid;
 		if (sdata->vif.type != NL80211_IFTYPE_STATION)

versions
@@ -2,4 +2,4 @@ BACKPORTS_VERSION="(see git)"
 BACKPORTED_KERNEL_VERSION="(see git)"
 BACKPORTED_KERNEL_NAME="iwlwifi"
 BACKPORTS_BUILD_TSTAMP=__DATE__ \" \" __TIME__
-BACKPORTS_GIT_TRACKED="iwlwifi-stack-public:master:8606:638a0863"
+BACKPORTS_GIT_TRACKED="iwlwifi-stack-public:master:8607:2fcf474b"