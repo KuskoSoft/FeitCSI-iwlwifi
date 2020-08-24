38b5def2b601f3910ba91096e4223afc2d54e756
Johannes Berg <johannes.berg@intel.com>
2020-08-24 10:23:05 +0200

    [BUGFIX] cfg80211: correct scan check on P2P device stop
    The identical code for normal netdevs was adjusted for the
    second scan request that may happen for 6 GHz scans, but
    the one in the P2P device code wasn't. Fix that now.
    
    type=bugfix
    ticket=jira:WIFI-66751
    fixes=I956f91022ef81544688a61adea963fb6912a192e
    
    Change-Id: I3f0349e6dd55d0a9e712f0b9850f9bff2b3ea0a1
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/274524
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Peer, Ilan <ilan.peer@intel.com>
    Reviewed-by: Luciano Coelho <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: 1cd61d2b259fcedc5ce8da34680737d4b912ef60

--- net/wireless/core.c
+++ net/wireless/core.c
@@ -236,7 +236,9 @@ void cfg80211_stop_p2p_device(struct cfg80211_registered_device *rdev,
 	rdev->opencount--;
 
 	if (rdev->scan_req && rdev->scan_req->wdev == wdev) {
-		if (WARN_ON(!rdev->scan_req->notified))
+		if (WARN_ON(!rdev->scan_req->notified &&
+			    (!rdev->int_scan_req ||
+			     !rdev->int_scan_req->notified)))
 			rdev->scan_req->info.aborted = true;
 		___cfg80211_scan_done(rdev, false);
 	}
