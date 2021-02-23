5f4d40784d1d21f942c88654b27f6e08cd457fc1
Luca Coelho <luciano.coelho@intel.com>
2021-02-23 15:41:13 +0200

    Merge remote-tracking branch 'auto/master' into HEAD
    type=maint
    ticket=none
    
    Change-Id: I0f06338d0db528eb23240a4a22983fc78b6135d9
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    x-iwlwifi-stack-dev: 3016f75065876c16c445cdf2b59be4d77bd0589c

--- include/net/cfg80211.h
+++ include/net/cfg80211.h
@@ -4950,8 +4950,10 @@ struct wiphy_iftype_akm_suites {
 struct wiphy {
 	/* assign these fields before you register the wiphy */
 
-#define WIPHY_COMPAT_PAD_SIZE	2048
+#ifdef CPTCFG_CFG80211_WEXT
+#define WIPHY_COMPAT_PAD_SIZE	2304
 	u8 padding[WIPHY_COMPAT_PAD_SIZE];
+#endif
 
 	u8 perm_addr[ETH_ALEN];
 	u8 addr_mask[ETH_ALEN];
