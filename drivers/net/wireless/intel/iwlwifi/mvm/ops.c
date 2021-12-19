8a1b7f022e90255bae6e8f614e34e090dbdf552a
Luca Coelho <luciano.coelho@intel.com>
2021-12-19 11:01:54 +0200

    [NOUPSTREAM] iwlwifi: mvm: sync some cosmetic adjustments with upstream
    There were a couple of differences between stack-dev and upstream in
    iwl_mvm_start_get_nvm().  Adjust stack-dev accordingly.
    
    type=cleanup
    ticket=none
    
    Change-Id: I1c8bbfe40c2caa20156bad1cc488da70a5c66aaa
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/333287
    Reviewed-by: Grumbach, Emmanuel <emmanuel.grumbach@intel.com>
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: c77c94d532d364bf8acdf9e9ec24ed669e494cbb

--- drivers/net/wireless/intel/iwlwifi/mvm/ops.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/ops.c
@@ -829,7 +829,6 @@ static int iwl_mvm_start_get_nvm(struct iwl_mvm *mvm)
 	}
 
 get_nvm_from_fw:
-
 	rtnl_lock();
 	wiphy_lock(mvm->hw->wiphy);
 	mutex_lock(&mvm->mutex);
@@ -844,8 +843,7 @@ get_nvm_from_fw:
 
 	ret = iwl_run_init_mvm_ucode(mvm);
 	if (ret && ret != -ERFKILL)
-		iwl_fw_dbg_error_collect(&mvm->fwrt,
-					 FW_DBG_TRIGGER_DRIVER);
+		iwl_fw_dbg_error_collect(&mvm->fwrt, FW_DBG_TRIGGER_DRIVER);
 	if (!ret && iwl_mvm_is_lar_supported(mvm)) {
 		mvm->hw->wiphy->regulatory_flags |= REGULATORY_WIPHY_SELF_MANAGED;
 		ret = iwl_mvm_init_mcc(mvm);
