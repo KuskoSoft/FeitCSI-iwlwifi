60352ff18a692c5c68ddd99e81bb359b1fe90775
Avraham Stern <avraham.stern@intel.com>
2022-02-01 12:19:06 +0200

    iwlwifi: mvm: report hardware timestamps in RX/TX status
    For TM/FTM frames, report the hardware timestamps reported by the
    fw as part of the RX/TX status. Since the fw reports the timestamps
    in a dedicated notification (and not as part of the RX/TX status),
    hold the frame until the fw timestamps notification is received.
    Timestamping is enabled when a station is connected and disabled
    when disconnected. For AP interface, only the first station will
    have timestamping enabled since the fw only supports timestamping
    for one peer.
    
    type=feature
    ticket=jira:WIFI-173902
    
    Change-Id: I9bf12c8ecfb3f17253a13dc48a48647ddd6e7855
    Signed-off-by: Avraham Stern <avraham.stern@intel.com>
    Reviewed-on: https://git-amr-3.devtools.intel.com/gerrit/333963
    automatic-review: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: ec ger unix iil jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: f50e136b73afa9aa1a99a1cb5ae1682fffdbe102

--- drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
+++ drivers/net/wireless/intel/iwlwifi/mvm/mvm.h
@@ -809,6 +809,12 @@ struct ptp_data {
 };
 #endif
 
+struct iwl_time_sync_data {
+	struct sk_buff_head frame_list;
+	u8 peer_addr[ETH_ALEN];
+	bool active;
+};
+
 struct iwl_mvm {
 	/* for logger access */
 	struct device *dev;
@@ -1192,6 +1198,8 @@ struct iwl_mvm {
 	unsigned long last_reset_or_resume_time_jiffies;
 
 	bool sta_remove_requires_queue_remove;
+
+	struct iwl_time_sync_data time_sync;
 };
 
 /* Extract MVM priv from op_mode and _hw */
