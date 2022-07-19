torvalds/linux

commit bd363ee5330250b93cb1e0e16c1c54682fcbe595

wifi: mac80211: mlme: set sta.mlo correctly
Due to some changes and rebasing between different patches
this fell through the cracks; we need to set sta.mlo if the
connection is using MLO.

Signed-off-by: Johannes Berg <johannes.berg@intel.com>

net/mac80211/mlme.c
@@ -6402,6 +6402,8 @@ static int ieee80211_prep_connection(struct ieee80211_sub_if_data *sdata,
 			err = -ENOMEM;
 			goto out_err;
 		}
+
+		new_sta->sta.mlo = link_id >= 0;
 	}
 
 	/*

KuskoSoft/FeitCSI-iwlwifi

commit 3bcb0f998ff93fcce22752c9039ae6d9c4a75021

[BUGFIX] mac80211: mlme: set sta.mlo to mlo state
At this point, we've already changed link_id to be zero for
a non-MLO connection, so use the 'mlo' variable rather than
link ID to determine the MLO status of the station.

type=bugfix
fixes=I04fa6b58f232850e51076f4d3062409cf70bc251
fixes=I62ff41ffb4f60c7e191877ffb2c70a9c6e966b48
ticket=none

Change-Id: I92272a450166c702188112ff3b0ba3b6bde90e32
Fixes: bd363ee53302 ("wifi: mac80211: mlme: set sta.mlo correctly")
Fixes: 81151ce462e5 ("wifi: mac80211: support MLO authentication/association with one link")
Signed-off-by: Johannes Berg <johannes.berg@intel.com>
Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/14319
tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
x-iwlwifi-stack-dev: 3d7904ec7b7f6ef17024097ea840483df485a4c7

net/mac80211/mlme.c
@@ -6587,7 +6587,7 @@ static int ieee80211_prep_connection(struct ieee80211_sub_if_data *sdata,
 	}
 
 	if (!have_sta) {
-		if (link_id >= 0)
+		if (mlo)
 			new_sta = sta_info_alloc_with_link(sdata, ap_mld_addr,
 							   link_id, cbss->bssid,
 							   GFP_KERNEL);
@@ -6599,7 +6599,7 @@ static int ieee80211_prep_connection(struct ieee80211_sub_if_data *sdata,
 			goto out_err;
 		}
 
-		new_sta->sta.mlo = link_id >= 0;
+		new_sta->sta.mlo = mlo;
 	}
 
 	/*

versions
@@ -2,4 +2,4 @@ BACKPORTS_VERSION="(see git)"
 BACKPORTED_KERNEL_VERSION="(see git)"
 BACKPORTED_KERNEL_NAME="iwlwifi"
 BACKPORTS_BUILD_TSTAMP=__DATE__ \" \" __TIME__
-BACKPORTS_GIT_TRACKED="iwlwifi-stack-public:master:10347:657572cd"
+BACKPORTS_GIT_TRACKED="iwlwifi-stack-public:master:10348:3d7904ec"