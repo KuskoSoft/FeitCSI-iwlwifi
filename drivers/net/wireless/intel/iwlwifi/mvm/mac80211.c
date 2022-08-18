8f418e22358e8af9f89f4ebae50a0318fcb95f3a
Johannes Berg <johannes.berg@intel.com>
2022-08-18 22:03:29 +0200

    [BUGFIX] wifi: iwlwifi: mvm: properly track AP STA
    There's some (ifdef'ed) code here that can return success in
    the middle, so we cannot put the assignment after that code
    block, it needs to be before.
    
    type=bugfix
    fixes=I88e6d252cdb0a6e4581d7c5fd7cbc037b4fd40ae
    ticket=none
    
    Change-Id: Ib985d44d6c2be233427dd7ca245608073e49041c
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/18495
    automatic-review: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 35a1c5caeed72ec25bf09f23cd4c7dee914cdf48

--- drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
@@ -3844,6 +3844,9 @@ iwl_mvm_sta_state_notexist_to_none(struct iwl_mvm *mvm,
 
 	sta->max_rc_amsdu_len = 1;
 
+	if (vif->type == NL80211_IFTYPE_STATION && !sta->tdls)
+		mvmvif->ap_sta = sta;
+
 #ifdef CPTCFG_IWLWIFI_SUPPORT_DEBUG_OVERRIDES
 	if (mvm->trans->dbg_cfg.ht_dynamic_smps)
 		smps_mode = IEEE80211_SMPS_DYNAMIC;
@@ -3858,9 +3861,6 @@ iwl_mvm_sta_state_notexist_to_none(struct iwl_mvm *mvm,
 				    i);
 	}
 #endif
-
-	if (vif->type == NL80211_IFTYPE_STATION && !sta->tdls)
-		mvmvif->ap_sta = sta;
 
 	return 0;
 }
