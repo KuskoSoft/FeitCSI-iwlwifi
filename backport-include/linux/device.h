2abe10c3a410511472da35b55635f5a27db39989
Johannes Berg <johannes.berg@intel.com>
2023-02-07 22:13:39 +0100

    [NOUPSTREAM] backports: adjust namespace argument const
    In newer kernels this is const, in older versions it's
    not, add a backport for that.
    
    The spatch for backports can be
    
        @ns@
        identifier cls, fn;
        @@
        struct class cls = {
          .namespace = fn,
        ...
        };
    
        @@
        identifier ns.fn, d;
        attribute name __ns_const;
        @@
        -void *fn(const struct device *d)
        +void *fn(__ns_const struct device *d)
         { ... }
    
    type=maint
    ticket=none
    
    Change-Id: I5edb37ab0ff939f256d6bd4bd14dd00904020f83
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/41400
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Greenman, Gregory <gregory.greenman@intel.com>
    x-iwlwifi-stack-dev: 7113a71ab1364505b71a6c7a81b0f6753623eeef

--- backport-include/linux/device.h
+++ backport-include/linux/device.h
@@ -7,4 +7,10 @@
 	struct device_attribute dev_attr_##_name = __ATTR_RW_MODE(_name, 0600)
 #endif
 
+#if LINUX_VERSION_IS_LESS(6,2,0)
+#define __ns_const
+#else
+#define __ns_const const
+#endif
+
 #endif /* __BACKPORT_DEVICE_H_ */
