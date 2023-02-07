5ac289831701d28b9881d9b9b816b4774b3d7473
Johannes Berg <johannes.berg@intel.com>
2023-02-07 22:22:16 +0100

    backport: add get_random_u32_inclusive()
    For get_random_u32_inclusive() we need get_random_u32_below(),
    implement it with prandom_u32_max() as it used to be in iwlwifi.
    
    type=maint
    ticket=none
    
    Change-Id: Id8abc5572c1a7910c760e5f9c418b9e41be79a32
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/41401
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Greenman, Gregory <gregory.greenman@intel.com>
    x-iwlwifi-stack-dev: f1137112bc7caac208df56c36af444acb182199f

--- backport-include/linux/random.h
+++ backport-include/linux/random.h
@@ -18,4 +18,19 @@ static inline u16 get_random_u16(void)
 }
 #endif
 
+#if LINUX_VERSION_IS_LESS(6,2,0)
+static inline u32 get_random_u32_below(u32 ceil)
+{
+	return prandom_u32_max(ceil);
+}
+
+static inline u32 get_random_u32_inclusive(u32 floor, u32 ceil)
+{
+	BUILD_BUG_ON_MSG(__builtin_constant_p(floor) && __builtin_constant_p(ceil) &&
+			 (floor > ceil || ceil - floor == U32_MAX),
+			 "get_random_u32_inclusive() must take floor <= ceil");
+	return floor + get_random_u32_below(ceil - floor + 1);
+}
+#endif
+
 #endif /* __BACKPORT_RANDOM_H */
