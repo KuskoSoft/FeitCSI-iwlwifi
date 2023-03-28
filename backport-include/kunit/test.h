903221948d70f4e81b753077f180cf1b5637cb91
Benjamin Berg <benjamin.berg@intel.com>
2023-03-28 19:06:02 +0200

    backport: kunit: add KUNIT_ARRAY_PARAM_DESC
    This will be included upstream in 6.4 hopefully.
    
    type=feature
    ticket=none
    
    Change-Id: I2699899eef0652edf539febc8c13e24f64148c29
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/49571
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Berg, Johannes <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: 877a81204463a06e0e6281f4de5b26b65ba932b2

---
+++ backport-include/kunit/test.h
@@ -0,0 +1,28 @@
+#ifndef __BP_KUNIT_TEST_H
+#define __BP_KUNIT_TEST_H
+#include <linux/version.h>
+
+#include_next <kunit/test.h>
+
+#if LINUX_VERSION_IS_LESS(6,4,0)
+/**
+ * KUNIT_ARRAY_PARAM_DESC() - Define test parameter generator from an array.
+ * @name:  prefix for the test parameter generator function.
+ * @array: array of test parameters.
+ * @desc_member: structure member from array element to use as description
+ *
+ * Define function @name_gen_params which uses @array to generate parameters.
+ */
+#define KUNIT_ARRAY_PARAM_DESC(name, array, desc_member)					\
+	static const void *name##_gen_params(const void *prev, char *desc)			\
+	{											\
+		typeof((array)[0]) *__next = prev ? ((typeof(__next)) prev) + 1 : (array);	\
+		if (__next - (array) < ARRAY_SIZE((array))) {					\
+			strscpy(desc, __next->desc_member, KUNIT_PARAM_DESC_SIZE);		\
+			return __next;								\
+		}										\
+		return NULL;									\
+	}
+#endif /* LINUX_VERSION_IS_LESS(6,4,0) */
+
+#endif /* __BP_KUNIT_TEST_H */
