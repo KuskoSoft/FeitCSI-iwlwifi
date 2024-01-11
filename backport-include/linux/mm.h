2e171efc297d891d4bc9006e3699f87463ad1d8f
Johannes Berg <johannes.berg@intel.com>
2024-01-11 13:31:16 +0100

    wifi: iwlwifi: fw: allow vmalloc for PNVM image
    This image can be pretty big (I've seen order-7 allocations!),
    and we later have to copy it to DMA memory (in newer FW even
    there it won't need to be contiguous), so we can easily deal
    with it being in vmalloc. Use kvmemdup()/kvfree() for it.
    
    type=cleanup
    ticket=none
    
    Change-Id: Idf699b36ec95ee36f530355cd2cb1da297a098f1
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/89386
    tested: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Korenblit, Miriam Rachel <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: bad08968bc7113d0edb5e6c19c4a2d65f0e4af6c

--- backport-include/linux/mm.h
+++ backport-include/linux/mm.h
@@ -53,4 +53,17 @@ static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
 }
 #endif /* < 4.18 */
 
+#if LINUX_VERSION_IS_LESS(6,3,0)
+#define kvmemdup LINUX_BACKPORT(kvmemdup)
+static inline void *kvmemdup(const void *src, size_t len, gfp_t gfp)
+{
+	void *p;
+
+	p = kvmalloc(len, gfp);
+	if (p)
+		memcpy(p, src, len);
+	return p;
+}
+#endif
+
 #endif /* __BACKPORT_MM_H */
