e16ecd2ea381cf2126bf0693987af84aa549a957
Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
2024-03-04 12:40:14 +0200

    wifi: iwlwifi: Add support for LARI_CONFIG_CHANGE_CMD cmd v9
    There is a requirement from OEMs to support new bits in DSM function 7,
    which will indicate enablement of 5.9 GHz in Canada.
    Add support for this by reading those bits from BIOS and sending it to the
    FW. mask unii4 allow bitmap based on LARI_CONFIG_CHANGE_CMD version
    
    type=feature
    ticket=jira:WIFI-380771
    
    Signed-off-by: Anjaneyulu <pagadala.yesu.anjaneyulu@intel.com>
    Change-Id: I0363992efc3607368648d34a7918b2534150a3ca
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/97311
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Mukesh Sisodiya <mukesh.sisodiya@intel.com>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: caa475590c2c110be7ca0bdad61d4fdd005f9f4d

--- drivers/net/wireless/intel/iwlwifi/xvt/xvt.c
+++ drivers/net/wireless/intel/iwlwifi/xvt/xvt.c
@@ -945,8 +945,14 @@ void iwl_xvt_lari_cfg(struct iwl_xvt *xvt)
 	cmd.config_bitmap = iwl_get_lari_config_bitmap(&xvt->fwrt);
 
 	ret = iwl_bios_get_dsm(&xvt->fwrt, DSM_FUNC_ENABLE_UNII4_CHAN, &value);
-	if (!ret)
+	if (!ret) {
+		if (cmd_ver < 9)
+			value &= DSM_UNII4_ALLOW_BITMAP_CMD_V8;
+		else
+			value &= DSM_UNII4_ALLOW_BITMAP;
+
 		cmd.oem_unii4_allow_bitmap = cpu_to_le32(value);
+	}
 
 	ret = iwl_bios_get_dsm(&xvt->fwrt, DSM_FUNC_ACTIVATE_CHANNEL, &value);
 	if (!ret) {
@@ -967,6 +973,7 @@ void iwl_xvt_lari_cfg(struct iwl_xvt *xvt)
 		size_t cmd_size;
 
 		switch (cmd_ver) {
+		case 9:
 		case 8:
 		case 7:
 			cmd_size = sizeof(struct iwl_lari_config_change_cmd_v7);
