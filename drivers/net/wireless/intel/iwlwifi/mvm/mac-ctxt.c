a08c670d387698d902d8f95a0f98754ab80f186d
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2024-05-08 08:46:57 +0300

    Revert "[BUGFIX][NOUPSTREAM]iwlwifi: mvm: temporary WA to make a000 family use new beacon template"
    This TLV is now updated in FW.
    This reverts commit a05e42fc1a59981ca41e46b919245bcf88ccac6f.
    
    type=main
    ticket=none
    
    Change-Id: I34a1f2939c6eaf622c48ea368c26d68f44483636
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/107993
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: 5ecd2b479ca638ec2d4fac671ab0203178f2f9c5

--- drivers/net/wireless/intel/iwlwifi/mvm/mac-ctxt.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mac-ctxt.c
@@ -1201,9 +1201,7 @@ static int iwl_mvm_mac_ctxt_send_beacon(struct iwl_mvm *mvm,
 			 IWL_UCODE_TLV_CAPA_CSA_AND_TBTT_OFFLOAD))
 		return iwl_mvm_mac_ctxt_send_beacon_v6(mvm, vif, beacon);
 
-	/* TODO: remove first condition once FW merge new TLV */
-	if (iwl_mvm_has_new_tx_api(mvm) ||
-	    fw_has_api(&mvm->fw->ucode_capa,
+	if (fw_has_api(&mvm->fw->ucode_capa,
 		       IWL_UCODE_TLV_API_NEW_BEACON_TEMPLATE))
 		return iwl_mvm_mac_ctxt_send_beacon_v9(mvm, vif, beacon,
 						       link_conf);
