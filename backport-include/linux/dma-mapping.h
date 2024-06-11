e73200db4cb3e80f5c3d12e5d0676f628d26e711
Benjamin Berg <benjamin.berg@intel.com>
2024-06-11 16:26:11 +0200

    backports: dma-mapping: add helpers for mapping sgtable objects
    Upstream commit d9d200bcebc1 ("dma-mapping: add generic helpers for
    mapping sgtable objects") added some helpers to map and unmap a
    struct sg_table instead of passing the contained scatter list.
    
    Add the helpers to backports.
    
    type=feature
    ticket=none
    
    Change-Id: I1fd33fbca111296d376df19e9f223b0d242de7dc
    Signed-off-by: Benjamin Berg <benjamin.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/115407
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    Reviewed-by: Johannes Berg <johannes.berg@intel.com>
    x-iwlwifi-stack-dev: a0bdc741b1a27413e55bfa1bfb9745b231680fbf

--- backport-include/linux/dma-mapping.h
+++ backport-include/linux/dma-mapping.h
@@ -16,4 +16,38 @@
 #define DMA_MAPPING_ERROR		(~(dma_addr_t)0)
 #endif /* DMA_MAPPING_ERROR */
 
+#if LINUX_VERSION_IS_LESS(5, 7, 0)
+
+static inline int dma_map_sgtable(struct device *dev, struct sg_table *sgt,
+		enum dma_data_direction dir, unsigned long attrs)
+{
+	int nents;
+
+	nents = dma_map_sg_attrs(dev, sgt->sgl, sgt->orig_nents, dir, attrs);
+	if (nents <= 0)
+		return -EINVAL;
+	sgt->nents = nents;
+	return 0;
+}
+
+static inline void dma_unmap_sgtable(struct device *dev, struct sg_table *sgt,
+		enum dma_data_direction dir, unsigned long attrs)
+{
+	dma_unmap_sg_attrs(dev, sgt->sgl, sgt->orig_nents, dir, attrs);
+}
+
+static inline void dma_sync_sgtable_for_cpu(struct device *dev,
+		struct sg_table *sgt, enum dma_data_direction dir)
+{
+	dma_sync_sg_for_cpu(dev, sgt->sgl, sgt->orig_nents, dir);
+}
+
+static inline void dma_sync_sgtable_for_device(struct device *dev,
+		struct sg_table *sgt, enum dma_data_direction dir)
+{
+	dma_sync_sg_for_device(dev, sgt->sgl, sgt->orig_nents, dir);
+}
+
+#endif /* LINUX_VERSION_IS_LESS(5, 7, 0) */
+
 #endif /* __BACKPORT_LINUX_DMA_MAPPING_H */
