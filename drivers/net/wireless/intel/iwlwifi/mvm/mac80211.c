32eb0f203077a63931237a443d84dce5c0ad51c6
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2024-06-16 09:53:22 +0300

    [BUGFIX] wifi: iwlwifi: mvm: avoid NULL dereference
    iwl_mvm_get_bss_vif might return a NULL or ERR_PTR. Some of the callers
    check only the NULL case, and some doesn't check at all.
    
    Some of the callers even have a pointer to the mvmvif of the bss vif,
    so we don't even need to call this function, and can simply get the vif
    from mvmvif. Do it for those cases, and for the others - properly check
    if IS_ERR_OR_NULL
    
    type=bugfix
    ticket=none
    fixes=If9eb946e929a39e10fe5f4638bc8bc3f8976edf1
    fixes=I7a8a5b24658744ed732bfc03b1872c9298483d62
    fixes=I6406db40cf25eabdba602afd783466473b909216
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I45cf09b01af8ee3d55828863958ead741ea43b7f
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/115775
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 5d1d70b8ae158ef1fa409ef57231bd5a41e64879

--- drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/mac80211.c
@@ -1791,7 +1791,8 @@ static void iwl_mvm_prevent_esr_done_wk(struct wiphy *wiphy,
 	struct iwl_mvm_vif *mvmvif =
 		container_of(wk, struct iwl_mvm_vif, prevent_esr_done_wk.work);
 	struct iwl_mvm *mvm = mvmvif->mvm;
-	struct ieee80211_vif *vif = iwl_mvm_get_bss_vif(mvm);
+	struct ieee80211_vif *vif =
+		container_of((void *)mvmvif, struct ieee80211_vif, drv_priv);
 
 	guard(mvm)(mvm);
 	iwl_mvm_unblock_esr(mvm, vif, IWL_MVM_ESR_BLOCKED_PREVENTION);
@@ -1813,7 +1814,8 @@ static void iwl_mvm_unblock_esr_tpt(struct wiphy *wiphy, struct wiphy_work *wk)
 	struct iwl_mvm_vif *mvmvif =
 		container_of(wk, struct iwl_mvm_vif, unblock_esr_tpt_wk);
 	struct iwl_mvm *mvm = mvmvif->mvm;
-	struct ieee80211_vif *vif = iwl_mvm_get_bss_vif(mvm);
+	struct ieee80211_vif *vif =
+		container_of((void *)mvmvif, struct ieee80211_vif, drv_priv);
 
 	guard(mvm)(mvm);
 	iwl_mvm_unblock_esr(mvm, vif, IWL_MVM_ESR_BLOCKED_TPT);
