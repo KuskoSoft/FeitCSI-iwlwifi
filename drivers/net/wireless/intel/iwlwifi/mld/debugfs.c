65a307a5f41cfd34a4dee9374f942e666c4aad6a
Daniel Gabay <daniel.gabay@intel.com>
2024-06-19 10:28:51 +0300

    wifi: iwlwifi: mld: add debugfs hook to cause firmware error
    Add debugfs hook for sending REPLY_ERROR command to cause a
    firmware error for testing restart/recovery flow.
    
    type=feature
    ticket=jira:WIFI-418847
    
    Signed-off-by: Daniel Gabay <daniel.gabay@intel.com>
    Change-Id: I34044d82f1ccce739be52ef7b08212a05b3b3a9a
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/116684
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 4c534882937dae80f5b2cdbbb461454de3fc5ffc

--- drivers/net/wireless/intel/iwlwifi/mld/debugfs.c
+++ drivers/net/wireless/intel/iwlwifi/mld/debugfs.c
@@ -6,6 +6,7 @@
 #include "mld.h"
 #include "debugfs.h"
 #include "iwl-io.h"
+#include "hcmd.h"
 
 #define MLD_DEBUGFS_READ_FILE_OPS(name, bufsz)				\
 	_MLD_DEBUGFS_READ_FILE_OPS(name, bufsz, struct iwl_mld)
@@ -39,7 +40,37 @@ static ssize_t iwl_dbgfs_fw_nmi_write(struct iwl_mld *mld, char *buf,
 	return count;
 }
 
+static ssize_t iwl_dbgfs_fw_restart_write(struct iwl_mld *mld, char *buf,
+					  size_t count)
+{
+	int __maybe_unused ret;
+
+	if (!iwlwifi_mod_params.fw_restart)
+		return -EPERM;
+
+	if (!mld->fw_status.running)
+		return -EIO;
+
+	wiphy_lock(mld->wiphy);
+
+	if (count == 6 && !strcmp(buf, "nolog\n")) {
+		mld->fw_status.do_not_dump_once = true;
+		set_bit(STATUS_SUPPRESS_CMD_ERROR_ONCE, &mld->trans->status);
+	}
+
+	/* take the return value to make compiler happy - it will
+	 * fail anyway
+	 */
+	ret = iwl_mld_send_cmd_pdu(mld, WIDE_ID(LONG_GROUP, REPLY_ERROR),
+				   0, NULL);
+
+	wiphy_unlock(mld->wiphy);
+
+	return count;
+}
+
 MLD_DEBUGFS_WRITE_FILE_OPS(fw_nmi, 10);
+MLD_DEBUGFS_WRITE_FILE_OPS(fw_restart, 10);
 
 void
 iwl_mld_add_debugfs_files(struct iwl_mld *mld, struct dentry *debugfs_dir)
@@ -47,6 +78,7 @@ iwl_mld_add_debugfs_files(struct iwl_mld *mld, struct dentry *debugfs_dir)
 	/* Add debugfs files here */
 
 	MLD_DEBUGFS_ADD_FILE(fw_nmi, debugfs_dir, 0200);
+	MLD_DEBUGFS_ADD_FILE(fw_restart, debugfs_dir, 0200);
 
 	/*
 	 * TODO: Once registered to mac80211, add a symlink in mac80211
