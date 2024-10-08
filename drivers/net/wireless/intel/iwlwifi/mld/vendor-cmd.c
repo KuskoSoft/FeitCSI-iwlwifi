7313d10667d5a129f74d6153bca0d1e37108cd13
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2024-10-08 13:01:55 +0300

    wifi: iwlwifi: mld: support IWL_MVM_VENDOR_CMD_SET_SAR_PROFILE
    Dumb copy paste from iwlmvm.
    
    type=feature
    ticket=none
    
    Change-Id: I3c66656d3a0de8ddeb30712cf5f82edf30e3d54a
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/143010
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 0c358d6d74b3b21a323a144555d3e1d4e7076ad1

--- drivers/net/wireless/intel/iwlwifi/mld/vendor-cmd.c
+++ drivers/net/wireless/intel/iwlwifi/mld/vendor-cmd.c
@@ -7,12 +7,15 @@
 #include <net/mac80211.h>
 #include "mld.h"
 #include "mcc.h"
+#include "regulatory.h"
 #include "vendor-cmd.h"
 #include "iwl-vendor-cmd.h"
 
 static const struct nla_policy
 iwl_mld_vendor_attr_policy[NUM_IWL_MVM_VENDOR_ATTR] = {
 	[IWL_MVM_VENDOR_ATTR_COUNTRY] = { .type = NLA_STRING, .len = 2 },
+	[IWL_MVM_VENDOR_ATTR_SAR_CHAIN_A_PROFILE] = { .type = NLA_U8 },
+	[IWL_MVM_VENDOR_ATTR_SAR_CHAIN_B_PROFILE] = { .type = NLA_U8 },
 };
 
 static struct nlattr **iwl_mld_parse_vendor_data(const void *data, int data_len)
@@ -72,6 +75,57 @@ free:
 	return retval;
 }
 
+static int iwl_mld_vendor_set_dynamic_txp_profile(struct wiphy *wiphy,
+						  struct wireless_dev *wdev,
+						  const void *data,
+						  int data_len)
+{
+	struct ieee80211_hw *hw = wiphy_to_ieee80211_hw(wiphy);
+	struct iwl_mld *mld = IWL_MAC80211_GET_MLD(hw);
+	struct nlattr **tb;
+	u8 chain_a, chain_b;
+	int err;
+
+	tb = iwl_mld_parse_vendor_data(data, data_len);
+	if (IS_ERR(tb))
+		return PTR_ERR(tb);
+
+	if (!tb[IWL_MVM_VENDOR_ATTR_SAR_CHAIN_A_PROFILE] ||
+	    !tb[IWL_MVM_VENDOR_ATTR_SAR_CHAIN_B_PROFILE]) {
+		kfree(tb);
+		return -EINVAL;
+	}
+
+	chain_a = nla_get_u8(tb[IWL_MVM_VENDOR_ATTR_SAR_CHAIN_A_PROFILE]);
+	chain_b = nla_get_u8(tb[IWL_MVM_VENDOR_ATTR_SAR_CHAIN_B_PROFILE]);
+
+	kfree(tb);
+
+	if (mld->fwrt.sar_chain_a_profile == chain_a &&
+	    mld->fwrt.sar_chain_b_profile == chain_b)
+		return 0;
+
+	mld->fwrt.sar_chain_a_profile = chain_a;
+	mld->fwrt.sar_chain_b_profile = chain_b;
+
+	/* If the fw is not running, settings will be applied upon fw load */
+	if (!mld->fw_status.running)
+		return 0;
+
+	err = iwl_mld_config_sar_profile(mld, chain_a, chain_b);
+
+	if (err > 0)
+		/* For SAR validation purpose we need to track the exact return
+		 * value of iwl_mld_sar_select_profile, mostly to differentiate
+		 * between general SAR failure and the case of WRDS disable
+		 * (it is illegal if WRDS doesn't exist but WGDS does).
+		 * Since nl80211 forbids a positive number as a return value,
+		 * in case SAR is disabled overwrite it with -ENOENT.
+		 */
+		err = -ENOENT;
+	return err;
+}
+
 static const struct wiphy_vendor_command iwl_mld_vendor_commands[] = {
 	{
 		.info = {
@@ -81,6 +135,16 @@ static const struct wiphy_vendor_command iwl_mld_vendor_commands[] = {
 		.flags = WIPHY_VENDOR_CMD_NEED_NETDEV |
 			 WIPHY_VENDOR_CMD_NEED_RUNNING,
 		.doit = iwl_mld_set_country,
+		.policy = iwl_mld_vendor_attr_policy,
+		.maxattr = MAX_IWL_MVM_VENDOR_ATTR,
+	},
+	{
+		.info = {
+			.vendor_id = INTEL_OUI,
+			.subcmd = IWL_MVM_VENDOR_CMD_SET_SAR_PROFILE,
+		},
+		.flags = WIPHY_VENDOR_CMD_NEED_WDEV,
+		.doit = iwl_mld_vendor_set_dynamic_txp_profile,
 		.policy = iwl_mld_vendor_attr_policy,
 		.maxattr = MAX_IWL_MVM_VENDOR_ATTR,
 	},
