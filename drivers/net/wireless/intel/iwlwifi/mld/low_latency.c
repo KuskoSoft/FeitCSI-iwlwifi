e4e80fd033242662a88e7ec6aa9288da7647ce8e
Daniel Gabay <daniel.gabay@intel.com>
2024-11-26 03:10:28 +0200

    wifi: iwlwifi: mld: implement low-latency worker
    The worker checks if the total TX/RX voice or video packets (per-MAC)
    reach the threshold (100 packets) within a 10-seconds period and
    updates the vif low-latency mode based on traffic indication (if
    something changed).
    
    Additionally, initialize the per-mac window start timestamps only
    after a voice or video packet is received or transmitted. This
    approach saves time in the low-latency worker by skipping
    mac id's that have no voice or video traffic at all.
    
    type=feature
    ticket=jira:WIFI-499274
    
    Signed-off-by: Daniel Gabay <daniel.gabay@intel.com>
    Change-Id: Idb7f208326671012e7dff1043eb428696d88ca3c
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/155532
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 38aed8f937e546ed80ffac762d927bc3e03a7581

--- drivers/net/wireless/intel/iwlwifi/mld/low_latency.c
+++ drivers/net/wireless/intel/iwlwifi/mld/low_latency.c
@@ -10,25 +10,125 @@
 
 #define MLD_LL_WK_INTERVAL_MSEC 500
 #define MLD_LL_PERIOD (HZ * MLD_LL_WK_INTERVAL_MSEC / 1000)
+#define MLD_LL_ACTIVE_WK_PERIOD (HZ * 10)
+
+/* packets/MLD_LL_WK_PERIOD seconds */
+#define MLD_LL_ENABLE_THRESH 100
+
+static bool iwl_mld_calc_low_latency(struct iwl_mld *mld,
+				     unsigned long timestamp)
+{
+	struct iwl_mld_low_latency *ll = &mld->low_latency;
+	bool global_low_latency = false;
+	u8 num_rx_q = mld->trans->num_rx_queues;
+
+	for (int mac_id = 0; mac_id < NUM_MAC_INDEX_DRIVER; mac_id++) {
+		u32 total_vo_vi_pkts = 0;
+		bool ll_period_expired;
+
+		/* If it's not initialized yet, it means we have not yet
+		 * received/transmitted any vo/vi packet on this MAC.
+		 */
+		if (!ll->window_start[mac_id])
+			continue;
+
+		ll_period_expired =
+			time_after(timestamp, ll->window_start[mac_id] +
+				   MLD_LL_ACTIVE_WK_PERIOD);
+
+		if (ll_period_expired)
+			ll->window_start[mac_id] = timestamp;
+
+		for (int q = 0; q < num_rx_q; q++) {
+			struct iwl_mld_low_latency_packets_counters *counters =
+				&mld->low_latency.pkts_counters[q];
+
+			spin_lock_bh(&counters->lock);
+
+			total_vo_vi_pkts += counters->vo_vi[mac_id];
+
+			if (ll_period_expired)
+				counters->vo_vi[mac_id] = 0;
+
+			spin_unlock_bh(&counters->lock);
+		}
+
+		/* enable immediately with enough packets but defer
+		 * disabling only if the low-latency period expired and
+		 * below threshold.
+		 */
+		if (total_vo_vi_pkts > MLD_LL_ENABLE_THRESH)
+			mld->low_latency.result[mac_id] = true;
+		else if (ll_period_expired)
+			mld->low_latency.result[mac_id] = false;
+
+		global_low_latency |= mld->low_latency.result[mac_id];
+	}
+
+	return global_low_latency;
+}
+
+static void iwl_mld_low_latency_iter(void *_data, u8 *mac,
+				     struct ieee80211_vif *vif)
+{
+	struct iwl_mld *mld = _data;
+	struct iwl_mld_vif *mld_vif = iwl_mld_vif_from_mac80211(vif);
+	bool prev = mld_vif->low_latency_causes & LOW_LATENCY_TRAFFIC;
+	bool low_latency;
+
+	if (WARN_ON(mld_vif->fw_id >= ARRAY_SIZE(mld->low_latency.result)))
+		return;
+
+	low_latency = mld->low_latency.result[mld_vif->fw_id];
+
+	if (prev != low_latency)
+		iwl_mld_vif_update_low_latency(mld, vif, low_latency,
+					       LOW_LATENCY_TRAFFIC);
+}
 
 static void iwl_mld_low_latency_wk(struct wiphy *wiphy, struct wiphy_work *wk)
 {
-  /* TODO */
-}
+	struct iwl_mld *mld = container_of(wk, struct iwl_mld,
+					   low_latency.work.work);
+	unsigned long timestamp = jiffies;
+	bool low_latency_active;
 
-static void iwl_mld_low_latency_setup_timestamps(struct iwl_mld *mld)
-{
-	struct iwl_mld_low_latency *ll = &mld->low_latency;
-	unsigned long ts = jiffies;
+	if (mld->fw_status.in_hw_restart)
+		return;
 
-	ll->timestamp = jiffies;
-	for (int mac_id = 0; mac_id < NUM_MAC_INDEX_DRIVER; mac_id++)
-		ll->window_start[mac_id] = ts;
+	/* It is assumed that the work was scheduled only after checking
+	 * at least MLD_LL_PERIOD has passed since the last update.
+	 */
+
+	low_latency_active = iwl_mld_calc_low_latency(mld, timestamp);
+
+	/* Update the timestamp now after the low-latency calculation */
+	mld->low_latency.timestamp = timestamp;
+
+	/* If low-latency is active we need to force re-evaluation after
+	 * 10 seconds, so that we can disable low-latency when
+	 * the low-latency traffic ends.
+	 *
+	 * Otherwise, we don't need to run the work because there is nothing to
+	 * disable.
+	 *
+	 * Note that this has no impact on the regular scheduling of the
+	 * updates triggered by traffic - those happen whenever the
+	 * MLD_LL_PERIOD timeout expire.
+	 */
+	if (low_latency_active)
+		wiphy_delayed_work_queue(mld->wiphy, &mld->low_latency.work,
+					 MLD_LL_ACTIVE_WK_PERIOD);
+
+	ieee80211_iterate_active_interfaces(mld->hw,
+					    IEEE80211_IFACE_ITER_NORMAL,
+					    iwl_mld_low_latency_iter, mld);
 }
 
 int iwl_mld_low_latency_init(struct iwl_mld *mld)
 {
 	struct iwl_mld_low_latency *ll = &mld->low_latency;
+	unsigned long ts = jiffies;
 
 	ll->pkts_counters = kcalloc(mld->trans->num_rx_queues,
 				    sizeof(*ll->pkts_counters), GFP_KERNEL);
@@ -40,7 +140,11 @@ int iwl_mld_low_latency_init(struct iwl_mld *mld)
 
 	wiphy_delayed_work_init(&ll->work, iwl_mld_low_latency_wk);
 
-	iwl_mld_low_latency_setup_timestamps(mld);
+	ll->timestamp = ts;
+
+	/* The low-latency window_start will be initialized per-MAC on
+	 * the first vo/vi packet received/transmitted.
+	 */
 
 	return 0;
 }
@@ -66,13 +170,14 @@ void iwl_mld_low_latency_restart_cleanup(struct iwl_mld *mld)
 {
 	struct iwl_mld_low_latency *ll = &mld->low_latency;
 
+	ll->timestamp = jiffies;
+
+	memset(ll->window_start, 0, sizeof(ll->window_start));
 	memset(ll->result, 0, sizeof(ll->result));
 
 	for (int q = 0; q < mld->trans->num_rx_queues; q++)
 		memset(ll->pkts_counters[q].vo_vi, 0,
 		       sizeof(ll->pkts_counters[q].vo_vi));
-
-	iwl_mld_low_latency_setup_timestamps(mld);
 }
 
 static int iwl_mld_send_low_latency_cmd(struct iwl_mld *mld, bool low_latency,
@@ -162,6 +267,7 @@ void iwl_mld_low_latency_update_counters(struct iwl_mld *mld,
 					 u8 mac_id, u8 queue)
 {
 	struct iwl_mld_low_latency_packets_counters *counters;
+	unsigned long ts = jiffies ? jiffies : 1;
 
 	/* we should have failed op mode init if NULL */
 	if (WARN_ON_ONCE(!mld->low_latency.pkts_counters))
@@ -179,6 +285,10 @@ void iwl_mld_low_latency_update_counters(struct iwl_mld *mld,
 	spin_lock_bh(&counters->lock);
 	counters->vo_vi[mac_id]++;
 	spin_unlock_bh(&counters->lock);
+
+	/* Initialize the window_start on the first vo/vi packet */
+	if (!mld->low_latency.window_start[mac_id])
+		mld->low_latency.window_start[mac_id] = ts;
 
 	if (time_is_before_jiffies(mld->low_latency.timestamp + MLD_LL_PERIOD))
 		wiphy_delayed_work_queue(mld->wiphy, &mld->low_latency.work,
