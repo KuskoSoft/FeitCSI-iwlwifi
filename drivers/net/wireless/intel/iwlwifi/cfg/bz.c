ae0cffacf24b5c2be46d518c48e30b1f78d5d18b
Johannes Berg <johannes.berg@intel.com>
2024-11-27 13:36:51 +0200

    [BUGFIX] wifi: iwlwifi: cfg: no PNVM is required for JF/HR
    The older radios don't have PNVM even with newer MACs,
    so don't require that.
    
    type=bugfix
    fixes=I782513432d3dcbf801e8262522ded95302548e1c
    ticket=none
    
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Change-Id: I79767394e6c37e82a48369b114c04f85c26136ed
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/156099
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: c788f85b2b05dba5cc5f5ec62fc2d1e4639a6c28

--- drivers/net/wireless/intel/iwlwifi/cfg/bz.c
+++ drivers/net/wireless/intel/iwlwifi/cfg/bz.c
@@ -35,6 +35,9 @@
 #define IWL_GL_B_FM_B_FW_PRE		"iwlwifi-gl-b0-fm-b0"
 #define IWL_GL_C_FM_C_FW_PRE		"iwlwifi-gl-c0-fm-c0"
 
+#define IWL_BZ_A_HR_B_MODULE_FIRMWARE(api) \
+	IWL_BZ_A_HR_B_FW_PRE "-" __stringify(api) ".ucode"
+
 static const struct iwl_base_params iwl_bz_base_params = {
 	.eeprom_size = OTP_LOW_IMAGE_SIZE_32K,
 	.num_of_queues = 512,
@@ -163,8 +166,7 @@ const struct iwl_cfg iwl_cfg_gl = {
 	.num_rbds = IWL_NUM_RBDS_BZ_EHT,
 };
 
-IWL_FW_AND_PNVM(IWL_BZ_A_HR_B_FW_PRE, IWL_BZ_UCODE_API_MAX);
-IWL_FW_AND_PNVM(IWL_BZ_A_HR_B_FW_PRE, IWL_BZ_UCODE_API_MAX);
+MODULE_FIRMWARE(IWL_BZ_A_HR_B_MODULE_FIRMWARE(IWL_BZ_UCODE_API_MAX));
 IWL_FW_AND_PNVM(IWL_BZ_A_GF_A_FW_PRE, IWL_BZ_UCODE_API_MAX);
 IWL_FW_AND_PNVM(IWL_BZ_A_GF4_A_FW_PRE, IWL_BZ_UCODE_API_MAX);
 IWL_FW_AND_PNVM(IWL_BZ_A_FM_B_FW_PRE, IWL_BZ_UCODE_API_MAX);
