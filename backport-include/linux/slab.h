87b846a89a86b64e057179ad1ab98a201100c80f
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2024-12-01 12:01:15 +0200

    backports: backport __free_kfree
    __free_kfree is a cleanup function that can be be used with the __free
    attribute. It was added to kernel 6.5, and then was modified to support
    also ERR_PTRs in kernel 6.9.
    
    Backport it for older kernels.
    
    type=maint
    ticket=none
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: Ic28fba45f0ac97c499c0a0dbfa2f17eefaaaf0d3
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/156974
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: fe49b8c0bc386a4ee91a4216c209dbc2d74c2ffd

--- backport-include/linux/slab.h
+++ backport-include/linux/slab.h
@@ -7,4 +7,14 @@
 #define kfree_sensitive(x)	kzfree(x)
 #endif
 
+#ifdef __free_kfree
+#undef __free_kfree
+DEFINE_FREE(kfree, void *, if (!IS_ERR_OR_NULL(_T)) kfree(_T))
+#endif
+
+#if LINUX_VERSION_IS_LESS(6,5,0)
+#include <linux/cleanup.h>
+DEFINE_FREE(kfree, void *, if (!IS_ERR_OR_NULL(_T)) kfree(_T))
+#endif
+
 #endif /* __BACKPORT_SLAB_H */
