3cb3cb305bc0e53cbd575b31756627a00a3e6885
Johannes Berg <johannes.berg@intel.com>
2025-01-13 12:28:56 +0100

    wifi: cfg80211: move link reconfig parameters into a struct
    Add a new struct cfg80211_ml_reconf_req to collect the link
    reconfiguration parameters.
    
    type=feature
    ticket=none
    
    Change-Id: Id1a3b1092dc52d0d3731a8798522fdf2e052bf0b
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/167519
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 9edd2ad2c8b74313411947ea73787f6cc6bb0307

--- net/wireless/core.h
+++ net/wireless/core.h
@@ -4,7 +4,7 @@
  *
  * Copyright 2006-2010	Johannes Berg <johannes@sipsolutions.net>
  * Copyright 2015-2016	Intel Deutschland GmbH
- * Copyright (C) 2018-2024 Intel Corporation
+ * Copyright (C) 2018-2025 Intel Corporation
  */
 #ifndef __NET_WIRELESS_CORE_H
 #define __NET_WIRELESS_CORE_H
@@ -570,8 +570,8 @@ void cfg80211_wdev_release_link_bsses(struct wireless_dev *wdev, u16 link_mask);
 
 int cfg80211_assoc_ml_reconf(struct cfg80211_registered_device *rdev,
 			     struct net_device *dev,
-			     struct cfg80211_assoc_link *links,
-			     u16 rem_links);
+			     struct cfg80211_ml_reconf_req *req);
+
 /**
  * struct cfg80211_colocated_ap - colocated AP information
  *
