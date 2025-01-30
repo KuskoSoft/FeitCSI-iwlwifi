64dcae370719983f4e98626e6a016d0027f3a1b4
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-01-30 15:15:31 +0200

    [BUGFIX] wifi: iwlwifi: mld: avoid init-after-queue of some wiphy works
    During reconfig iwl_mld_init_vif is called, where all the EMLSR related
    wiphy works are initialized.
    But if one of those works was queued to run just after the reconfig work,
    and during the reconfig this work is getting initialized, we are getting
    to a init-after-queue situation.
    This corrupts of the wiphy work list, and therefore that work is
    executed again and again endlessly.
    
    type=bugfix
    ticket=jira:WIFI-570516
    fixes=unknown
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: Icbf44cd014a5de68d8f1538964faa9e703273921
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/172048
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: 0124a09d0977eb430fed261642eaa921ebf3457e

--- drivers/net/wireless/intel/iwlwifi/mld/iface.c
+++ drivers/net/wireless/intel/iwlwifi/mld/iface.c
@@ -387,14 +387,16 @@ iwl_mld_init_vif(struct iwl_mld *mld, struct ieee80211_vif *vif)
 	if (ret)
 		return ret;
 
-	wiphy_work_init(&mld_vif->emlsr.unblock_tpt_wk,
-			iwl_mld_emlsr_unblock_tpt_wk);
-	wiphy_delayed_work_init(&mld_vif->emlsr.check_tpt_wk,
-				iwl_mld_emlsr_check_tpt);
-	wiphy_delayed_work_init(&mld_vif->emlsr.prevent_done_wk,
-				iwl_mld_emlsr_prevent_done_wk);
-	wiphy_delayed_work_init(&mld_vif->emlsr.tmp_non_bss_done_wk,
-				iwl_mld_emlsr_tmp_non_bss_done_wk);
+	if (!mld->fw_status.in_hw_restart) {
+		wiphy_work_init(&mld_vif->emlsr.unblock_tpt_wk,
+				iwl_mld_emlsr_unblock_tpt_wk);
+		wiphy_delayed_work_init(&mld_vif->emlsr.check_tpt_wk,
+					iwl_mld_emlsr_check_tpt);
+		wiphy_delayed_work_init(&mld_vif->emlsr.prevent_done_wk,
+					iwl_mld_emlsr_prevent_done_wk);
+		wiphy_delayed_work_init(&mld_vif->emlsr.tmp_non_bss_done_wk,
+					iwl_mld_emlsr_tmp_non_bss_done_wk);
+	}
 
 	return 0;
 }
