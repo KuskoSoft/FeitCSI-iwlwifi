5c03fe713bbb6e89b5f3dd51005e93cf9593ceeb
Somashekhar Puttagangaiah <somashekhar.puttagangaiah@intel.com>
2025-02-22 14:38:48 +0200

    wifi: iwlwifi: mld: allow EMLSR with 2.4 GHz when BT is ON
    When BT is ON, EMLSR with one of the links operating on 2.4 GHz
    is allowed only if it meets following conditions.
    In this patch:
    1. during link selection, when BT is ON, allow emlsr only if BT
       pentalty is < 7%.
    2. exit EMLSR if BT is turned ON and one of the links is operating
       on 2.4 GHz with BT penalty > 7%
    
    type=feature
    ticket=jira:WIFI-582587
    
    Change-Id: I48e3d5bd6b0b8583f98057c38d2ee30fff5abd8a
    Signed-off-by: Somashekhar Puttagangaiah <somashekhar.puttagangaiah@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/177500
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: ab0fcddc889fdb37a59617fb350a4f12d16acd1a

--- drivers/net/wireless/intel/iwlwifi/mld/mld.h
+++ drivers/net/wireless/intel/iwlwifi/mld/mld.h
@@ -130,7 +130,6 @@
  *	cleanup using iwl_mld_free_internal_sta
  * @netdetect: indicates the FW is in suspend mode with netdetect configured
  * @p2p_device_vif: points to the p2p device vif if exists
- * @bt_is_active: indicates that BT is active
  * @dev: pointer to device struct. For printing purposes
  * @trans: pointer to the transport layer
  * @cfg: pointer to the device configuration
@@ -190,6 +189,7 @@
  * @time_sync: time sync data.
  * @ftm_initiator: FTM initiator data
  * @ftm_responder: FTM responder data
+ * @last_bt_notif: last received BT Coex notif
  */
 struct iwl_mld {
 	/* Add here fields that need clean up on restart */
@@ -213,7 +213,7 @@ struct iwl_mld {
 		bool netdetect;
 #endif /* CONFIG_PM_SLEEP */
 		struct ieee80211_vif *p2p_device_vif;
-		bool bt_is_active;
+		struct iwl_bt_coex_profile_notif last_bt_notif;
 	);
 	struct ieee80211_link_sta __rcu *fw_id_to_link_sta[IWL_STATION_COUNT_MAX];
 	/* And here fields that survive a fw restart */
