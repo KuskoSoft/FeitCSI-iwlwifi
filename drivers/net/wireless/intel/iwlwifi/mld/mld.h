b6c28e6cc88aed61a92042a6b35cd45bc650f92a
Miri Korenblit <miriam.rachel.korenblit@intel.com>
2025-02-23 09:00:32 +0200

    [BUGFIX] wifi: iwlwifi: mld: fix build with CONFIG_PM_SLEEP undefined
    fw_status.in_d3 is only defined under CONFIG_PM_SLEEP.
    
    type=bugfix
    ticket=none
    fixes=I9e3927a72d5df6a3226f320843d4e4a28ab74dd4
    fixes=Ie0226a339180be799104c8983086d417061aa07d
    
    Signed-off-by: Miri Korenblit <miriam.rachel.korenblit@intel.com>
    Change-Id: I78b9311019b59477a1961cddc4640b255ceda651
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/177644
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    x-iwlwifi-stack-dev: f8e878fbc8334a0ad56485b543103893391e7a10

--- drivers/net/wireless/intel/iwlwifi/mld/mld.h
+++ drivers/net/wireless/intel/iwlwifi/mld/mld.h
@@ -322,7 +322,10 @@ iwl_cleanup_mld(struct iwl_mld *mld)
 	CLEANUP_STRUCT(&mld->scan);
 
 	iwl_mld_cleanup_rfi(mld);
+
+#ifdef CONFIG_PM_SLEEP
 	mld->fw_status.in_d3 = false;
+#endif
 
 	iwl_mld_low_latency_restart_cleanup(mld);
 
