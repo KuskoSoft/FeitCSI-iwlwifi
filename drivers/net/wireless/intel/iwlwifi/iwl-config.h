d4f9812901c222e78ef1ae7d61121db6509e6ef3
Somashekhar Puttagangaiah <somashekhar.puttagangaiah@intel.com>
2025-03-05 11:48:54 +0200

    wifi: iwlwifi: pcie: Add support for new device ids
    Add device support for PeP on ScP
    
    type=feature
    ticket=none
    
    Change-Id: Icbbc7c28855f8de232cb3916901a4da3ac286117
    Signed-off-by: Somashekhar Puttagangaiah <somashekhar.puttagangaiah@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/180201
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Tested-by: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 78e30952737536c1b88567549a624680e2d0e922

--- drivers/net/wireless/intel/iwlwifi/iwl-config.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-config.h
@@ -552,6 +552,7 @@ extern const char iwl_bz_name[];
 extern const char iwl_fm_name[];
 extern const char iwl_wh_name[];
 extern const char iwl_sp_name[];
+extern const char iwl_pe_name[];
 extern const char iwl_gl_name[];
 extern const char iwl_mtp_name[];
 extern const char iwl_dr_name[];
