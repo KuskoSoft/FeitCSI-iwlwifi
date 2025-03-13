eb855f50b4c38f88e93c37855d53f3e70b297a0c
Emmanuel Grumbach <emmanuel.grumbach@intel.com>
2025-03-13 09:43:43 +0200

    [BUGFIX] wifi: iwlwifi: do not use iwlmld for non-wifi7 devices
    Roll-back to use iwlmvm for those devices. iwlmld will support wifi7
    capable devices only. The firmware for the non-wifi7 capable will soon
    be frozen and we don't want iwlmld to have to support devices that will
    require the old APIs.
    
    type=bugfix
    ticket=none
    fixes=Ic9b0bbe1be6bc5e2f4983cc5a47800f476c2f26a
    
    Fixes: d1e879ec600f9 ("wifi: iwlwifi: add iwlmld sub-driver")
    Change-Id: I0020ca9f6c6c928caa78721666df131a692c6186
    Signed-off-by: Emmanuel Grumbach <emmanuel.grumbach@intel.com>
    Reviewed-on: https://gerritwcs.ir.intel.com/c/iwlwifi-stack-dev/+/182068
    automatic-review: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    tested: iil_jenkins iil_jenkins <EC.GER.UNIX.IIL.JENKINS@INTEL.COM>
    Reviewed-by: Miriam Rachel Korenblit <miriam.rachel.korenblit@intel.com>
    x-iwlwifi-stack-dev: 87e5516dc6edc821e4b2658de0f22eb376d69546

--- drivers/net/wireless/intel/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-drv.c
@@ -390,6 +390,11 @@ static inline char iwl_drv_get_step(int step)
 	return 'a' + step;
 }
 
+static bool iwl_drv_is_wifi7_supported(struct iwl_trans *trans)
+{
+	return CSR_HW_RFID_TYPE(trans->hw_rf_id) >= IWL_CFG_RF_TYPE_FM;
+}
+
 const char *iwl_drv_get_fwname_pre(struct iwl_trans *trans, char *buf)
 {
 	char mac_step, rf_step;
@@ -2210,10 +2215,12 @@ static void iwl_req_fw_callback(const struct firmware *ucode_raw, void *context)
 	}
 
 #if IS_ENABLED(CPTCFG_IWLMLD)
-	if (pieces->major >= IWL_MLD_SUPPORTED_FW_VERSION)
+	if (pieces->major >= IWL_MLD_SUPPORTED_FW_VERSION &&
+	    iwl_drv_is_wifi7_supported(drv->trans))
 		op = &iwlwifi_opmode_table[MLD_OP_MODE];
 #else
-	if (pieces->major >= IWL_MLD_SUPPORTED_FW_VERSION) {
+	if (pieces->major >= IWL_MLD_SUPPORTED_FW_VERSION &&
+	    iwl_drv_is_wifi7_supported(drv->trans)) {
 		IWL_ERR(drv,
 			"IWLMLD needs to be compiled to support this firmware\n");
 		mutex_unlock(&iwlwifi_opmode_table_mtx);
